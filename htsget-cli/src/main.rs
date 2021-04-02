269b4c681ba18c8b7fc92df5e07e9e55616041e6
Christian Perez-Llamas <932644+chris-zen@users.noreply.github.com>
2021-04-02 12:43:37 +0200

    fixes & model improvements & bam search progress

--- htsget-cli/src/main.rs
+++ htsget-cli/src/main.rs
@@ -1,30 +1,32 @@
 use anyhow::Result;
-use clap::{App, Arg, SubCommand, ArgMatches};
+use clap::{App, Arg, ArgMatches, SubCommand};
 
 use htsget_search::htsget::{HtsGet, Query, Response};
 use htsget_search::simple::SimpleHtsGet;
 
 fn main() -> Result<()> {
-    let htsget = SimpleHtsGet::new("../data");
-    let args = unimplemented!();
-    htsget_search(&mut htsget, args)?;
+  let htsget = SimpleHtsGet::new("../data");
+  let args = unimplemented!();
+  htsget_search(&mut htsget, args)?;
 }
 
-fn htsget_search<HG>(htsget: &mut HG, args: &ArgMatches) -> Result<()> where HG: HtsGet {
+fn htsget_search<HG>(htsget: &mut HG, args: &ArgMatches) -> Result<()>
+where
+  HG: HtsGet,
+{
+  // let id = args.value_of("id").unwrap().to_string();
+  // TODO build the Query from the args ...
 
-    // let id = args.value_of("id").unwrap().to_string();
-    // TODO build the Query from the args ...
+  let query = Query::new("BroadHiSeqX_b37/NA12878")
+    .with_reference_name("11")
+    .with_start(5011963)
+    .with_end(5012660);
 
-    let query = Query::new("BroadHiSeqX_b37/NA12878")
-        .with_reference_name("11")
-        .with_start(5011963)
-        .with_end(5012660);
+  println!("Searching {:#?}: ", query);
 
-    println!("Searching {:#?}: ", query);
+  let response = htsget.search(query)?;
 
-    let response = htsget.search(query)?;
-    
-    println!("{:#?}", response);
+  println!("{:#?}", response);
 
-    Ok(())
+  Ok(())
 }
--- htsget-search/src/htsget.rs
+++ htsget-search/src/htsget.rs
@@ -2,86 +2,128 @@
 ///
 /// Based on the htsget spec: https://samtools.github.io/hts-specs/htsget.html
 ///
-
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum HtsGetError {
+  #[error("Not found: {0}")]
+  NotFound(String),
+
+  #[error("Unsupported Format: {0}")]
+  UnsupportedFormat(String),
+
   #[error("Invalid input: {0}")]
   InvalidInput(String),
-  
+
+  #[error("Invalid range: {0}")]
+  InvalidRange(String),
+
   #[error("IO error: {0}")]
   IOError(String),
 }
 
+impl HtsGetError {
+  pub fn not_found<S: Into<String>>(message: S) -> Self {
+    Self::NotFound(message.into())
+  }
+
+  pub fn unsupported_format<S: Into<String>>(format: S) -> Self {
+    Self::UnsupportedFormat(format.into())
+  }
+
+  pub fn invalid_input<S: Into<String>>(message: S) -> Self {
+    Self::InvalidInput(message.into())
+  }
+
+  pub fn invalid_range<S: Into<String>>(message: S) -> Self {
+    Self::InvalidRange(message.into())
+  }
+
+  pub fn io_error<S: Into<String>>(message: S) -> Self {
+    Self::IOError(message.into())
+  }
+}
+
 #[derive(Debug)]
 pub struct Query {
-    pub id: String,
-    pub format: Option<Format>,
-    pub class: Option<String>,
-    pub reference_name: Option<String>,
-    pub start: Option<u32>,
-    pub end: Option<u32>,
-    pub fields: Vec<String>,
-    pub tags: Option<Tags>,
-    pub no_tags: Option<Vec<String>>,
+  pub id: String,
+  pub format: Option<Format>,
+  pub class: Option<String>,
+  pub reference_name: Option<String>,
+  pub start: Option<u32>,
+  pub end: Option<u32>,
+  pub fields: Vec<String>,
+  pub tags: Option<Tags>,
+  pub no_tags: Option<Vec<String>>,
 }
 
 impl Query {
-    pub fn new(id: impl Into<String>) -> Self {
-        Self {
-            id: id.into(),
-            format: None,
-            class: None,
-            reference_name: None,
-            start: None,
-            end: None,
-            fields: Vec::new(),
-            tags: None,
-            no_tags: None,
-        }
+  pub fn new(id: impl Into<String>) -> Self {
+    Self {
+      id: id.into(),
+      format: None,
+      class: None,
+      reference_name: None,
+      start: None,
+      end: None,
+      fields: Vec::new(),
+      tags: None,
+      no_tags: None,
     }
+  }
 
-    pub fn with_format(mut self, format: Format) -> Self {
-        self.format = Some(format);
-        self
-    }
+  pub fn with_format(mut self, format: Format) -> Self {
+    self.format = Some(format);
+    self
+  }
 
-    pub fn with_class(mut self, class: impl Into<String>) -> Self {
-        self.class = Some(class.into());
-        self
-    }
+  pub fn with_class(mut self, class: impl Into<String>) -> Self {
+    self.class = Some(class.into());
+    self
+  }
 
-    pub fn with_reference_name(mut self, reference_name: impl Into<String>) -> Self {
-        self.reference_name = Some(reference_name.into());
-        self
-    }
+  pub fn with_reference_name(mut self, reference_name: impl Into<String>) -> Self {
+    self.reference_name = Some(reference_name.into());
+    self
+  }
 
-    pub fn with_start(mut self, start: u32) -> Self {
-        self.start = Some(start);
-        self
-    }
+  pub fn with_start(mut self, start: u32) -> Self {
+    self.start = Some(start);
+    self
+  }
 
-    pub fn with_end(mut self, end: u32) -> Self {
-        self.end = Some(end);
-        self
-    }
+  pub fn with_end(mut self, end: u32) -> Self {
+    self.end = Some(end);
+    self
+  }
 
-    // TODO the rest of the builder methods ...
+  // TODO the rest of the builder methods ...
 }
 
 #[derive(Debug)]
 pub enum Format {
-    BAM,
-    CRAM,
-    VCF,
-    BCF,
+  BAM,
+  CRAM,
+  VCF,
+  BCF,
+}
+
+impl Into<String> for Format {
+  fn into(self) -> String {
+    match self {
+      Self::BAM => "BAM",
+      Self::CRAM => "CRAM",
+      Self::VCF => "VCF",
+      Self::BCF => "BCF",
+    }
+    .to_string()
+  }
 }
 
 #[derive(Debug)]
 pub enum Tags {
-    All,
-    List(Vec<String>),
+  All,
+  List(Vec<String>),
 }
 
 #[derive(Debug)]
@@ -91,29 +133,29 @@ pub struct Headers {
 }
 
 impl Headers {
-    pub fn new(authorization: String, range: String) -> Self {
-        Self {
-            authorization,
-            range,
-        }
+  pub fn new(authorization: String, range: String) -> Self {
+    Self {
+      authorization,
+      range,
     }
+  }
 }
 
 #[derive(Debug)]
 pub struct Url {
   pub url: String,
-  pub headers: Headers,
-  pub class: String,
+  pub headers: Option<Headers>,
+  pub class: Option<String>,
 }
 
 impl Url {
-    pub fn new(url: String, headers: Headers, class: String) -> Self {
-        Self {
-            url,
-            headers,
-            class,
-        }
+  pub fn new(url: String, headers: Option<Headers>, class: Option<String>) -> Self {
+    Self {
+      url,
+      headers,
+      class,
     }
+  }
 }
 
 #[derive(Debug)]
@@ -123,5 +165,5 @@ pub struct Response {
 }
 
 pub trait HtsGet {
-    fn search(&self, query: Query) -> Result<Response, HtsGetError>;
+  fn search(&self, query: Query) -> Result<Response, HtsGetError>;
 }
