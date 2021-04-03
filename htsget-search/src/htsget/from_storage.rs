6942520ad00601f4ba591bb6ffd1dea526e6618a
Christian Perez-Llamas <932644+chris-zen@users.noreply.github.com>
2021-04-04 01:38:54 +0200

    added test for HtsGetFromStorage

--- htsget-search/src/htsget/from_storage.rs
+++ htsget-search/src/htsget/from_storage.rs
@@ -28,19 +28,45 @@ impl<S> HtsGetFromStorage<S> {
   pub fn new(storage: S) -> Self {
     Self { storage }
   }
+
+  pub fn storage(&self) -> &S {
+    &self.storage
+  }
 }
 
 #[cfg(test)]
 mod tests {
 
-  use crate::storage::local::LocalStorage;
+  use crate::htsget::bam::tests::with_local_storage;
+  use crate::htsget::{Headers, Url};
 
   use super::*;
 
   #[test]
-  fn search_() {
-    // TODO determine root path through cargo env vars
-    let storage = LocalStorage::new("../data");
-    let htsget = HtsGetFromStorage::new(storage);
+  fn search_bam() {
+    with_local_storage(|storage| {
+      let htsget = HtsGetFromStorage::new(storage);
+      let query = Query::new("htsnexus_test_NA12878").with_format(Format::BAM);
+      let response = htsget.search(query);
+      println!("{:#?}", response);
+      let expected_url = format!(
+        "file://{}",
+        htsget
+          .storage()
+          .base_path()
+          .join("htsnexus_test_NA12878.bam")
+          .to_string_lossy()
+      );
+      let expected_response = Ok(Response::new(
+        Format::BAM,
+        vec![
+          Url::new(expected_url.clone())
+            .with_headers(Headers::default().with_header("Range", "bytes=4668-977196")),
+          Url::new(expected_url)
+            .with_headers(Headers::default().with_header("Range", "bytes=977196-2112141")),
+        ],
+      ));
+      assert_eq!(response, expected_response)
+    })
   }
 }
