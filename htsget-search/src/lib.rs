73593e8d5ecb7458c3f9244c532f82b231265e51
Christian Perez Llamas <christian.perez@adevinta.com>
2021-04-03 17:49:19 +0200

    reorganized + rustdocs

--- htsget-search/src/lib.rs
+++ htsget-search/src/lib.rs
@@ -1,2 +1,5 @@
+//! Minimal Viable Product (MVP) to prove that an implementation of the [HtsGet specification](https://samtools.github.io/hts-specs/htsget.html) is worth using Rust.
+//!
+
 pub mod htsget;
 pub mod storage;
--- htsget-search/src/storage/mod.rs
+++ htsget-search/src/storage/mod.rs
@@ -1,9 +1,10 @@
-use std::path::PathBuf;
-use thiserror::Error;
-
-use crate::htsget::HtsGetError;
+//! Module providing the abstractions needed to read files from an storage
+//!
 
 pub mod local;
+
+use std::path::PathBuf;
+use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum StorageError {
@@ -16,11 +17,28 @@ pub enum StorageError {
 
 type Result<T> = core::result::Result<T, StorageError>;
 
+pub struct GetOptions {
+  max_length: Option<usize>,
+}
+
+impl GetOptions {
+  pub fn with_max_length(mut self, max_length: usize) -> Self {
+    self.max_length = Some(max_length);
+    self
+  }
+}
+
+impl Default for GetOptions {
+  fn default() -> Self {
+    Self { max_length: None }
+  }
+}
+
 /// An Storage represents some kind of object based storage (either locally or in the cloud)
 /// that can be used to retrieve files for alignments, variants or its respective indexes.
 pub trait Storage {
   // TODO Consider another type of interface based on IO streaming
   // so we don't need to guess the length of the headers, but just
   // parse them in an streaming fashion.
-  fn get<K: AsRef<str>>(&self, key: K, length: Option<usize>) -> Result<PathBuf>;
+  fn get<K: AsRef<str>>(&self, key: K, options: GetOptions) -> Result<PathBuf>;
 }
