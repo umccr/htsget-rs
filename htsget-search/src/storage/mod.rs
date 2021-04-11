189d5b5b3f5ff8d21bef58f3408cd44f04e475ae
Christian Pérez-Llamas <932644+chris-zen@users.noreply.github.com>
2021-04-11 12:04:22 +0200

    add tests for BytesRange (#14)

--- htsget-search/src/storage/mod.rs
+++ htsget-search/src/storage/mod.rs
@@ -19,7 +19,7 @@ pub enum StorageError {
 
 type Result<T> = core::result::Result<T, StorageError>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct BytesRange {
   start: Option<u64>,
   end: Option<u64>,
@@ -60,7 +60,7 @@ impl BytesRange {
     cond1 && cond2
   }
 
-  pub fn merge_with(&mut self, range: &BytesRange) {
+  pub fn merge_with(&mut self, range: &BytesRange) -> &Self {
     self.start = match (self.start.as_ref(), range.start.as_ref()) {
       (None, None) | (None, Some(_)) | (Some(_), None) => None,
       (Some(a), Some(b)) => Some(*a.min(b)),
@@ -69,6 +69,7 @@ impl BytesRange {
       (None, None) | (None, Some(_)) | (Some(_), None) => None,
       (Some(a), Some(b)) => Some(*a.max(b)),
     };
+    self
   }
 
   pub fn merge_all(mut ranges: Vec<BytesRange>) -> Vec<BytesRange> {
@@ -94,14 +95,14 @@ impl BytesRange {
 
       for range in ranges.iter().skip(1) {
         if current_range.overlaps(range) {
-          current_range.merge_with(range)
+          current_range.merge_with(range);
         } else {
           optimized_ranges.push(current_range.clone());
           current_range = range.clone();
         }
       }
 
-      optimized_ranges.push(current_range.clone());
+      optimized_ranges.push(current_range);
 
       optimized_ranges
     }
@@ -176,4 +177,305 @@ pub trait Storage {
   fn get<K: AsRef<str>>(&self, key: K, options: GetOptions) -> Result<PathBuf>;
 
   fn url<K: AsRef<str>>(&self, key: K, options: UrlOptions) -> Result<Url>;
+}
+
+#[cfg(test)]
+mod tests {
+
+  use super::*;
+
+  #[test]
+  fn bytes_range_overlapping_and_merge() {
+    let test_cases = vec![
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),    //         <--]
+        BytesRange::new(Some(3), Some(5)), //             [-]
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)), //            <--]
+        BytesRange::new(Some(3), None), //                [------>
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        Some(BytesRange::new(None, Some(4))), //      <----]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),    //         <--]
+        BytesRange::new(Some(2), None),    //            [------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(Some(1), Some(3)),    //        [-]
+        Some(BytesRange::new(None, Some(3))), //      <---]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),    //         <--]
+        BytesRange::new(Some(1), None),    //           [-------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(Some(0), Some(2)),    //       [-]
+        Some(BytesRange::new(None, Some(2))), //      <--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(None, Some(2)),       //      <--]
+        Some(BytesRange::new(None, Some(2))), //      <--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(Some(0), Some(1)),    //       []
+        Some(BytesRange::new(None, Some(2))), //      <--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),       //      <--]
+        BytesRange::new(None, Some(1)),       //      <-]
+        Some(BytesRange::new(None, Some(2))), //      <--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, Some(2)),    //         <--]
+        BytesRange::new(None, None),       //         <---------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)), //            [-]
+        BytesRange::new(Some(6), Some(8)), //                [-]
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)), //            [-]
+        BytesRange::new(Some(6), None),    //                [--->
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),       //      [-]
+        BytesRange::new(Some(4), Some(6)),       //        [-]
+        Some(BytesRange::new(Some(2), Some(6))), //      [---]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        BytesRange::new(Some(4), None),       //           [----->
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),       //      [-]
+        BytesRange::new(Some(3), Some(5)),       //       [-]
+        Some(BytesRange::new(Some(2), Some(5))), //      [--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        BytesRange::new(Some(3), None),       //          [------>
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),       //      [-]
+        BytesRange::new(Some(2), Some(3)),       //      []
+        Some(BytesRange::new(Some(2), Some(4))), //      [-]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        BytesRange::new(None, Some(3)),       //      <---]
+        Some(BytesRange::new(None, Some(4))), //      <----]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),       //      [-]
+        BytesRange::new(Some(1), Some(3)),       //     [-]
+        Some(BytesRange::new(Some(1), Some(4))), //     [--]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        BytesRange::new(None, Some(3)),       //      <---]
+        Some(BytesRange::new(None, Some(4))), //      <----]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),       //      [-]
+        BytesRange::new(Some(0), Some(2)),       //    [-]
+        Some(BytesRange::new(Some(0), Some(4))), //    [---]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        BytesRange::new(None, Some(2)),       //      <--]
+        Some(BytesRange::new(None, Some(4))), //      <----]
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)), //            [-]
+        BytesRange::new(Some(0), Some(1)), //          []
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)), //            [-]
+        BytesRange::new(None, Some(1)),    //         <-]
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), Some(4)), //            [-]
+        BytesRange::new(None, None),       //         <---------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(4), Some(6)),    //           [-]
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(4), None),       //           [----->
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(2), Some(4)),    //         [-]
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(2), None),       //         [------->
+        Some(BytesRange::new(Some(2), None)), //         [------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(1), Some(3)),    //        [-]
+        Some(BytesRange::new(Some(1), None)), //        [-------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),    //            [------->
+        BytesRange::new(None, Some(3)),    //         <---]
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),       //         [------->
+        BytesRange::new(Some(0), Some(2)),    //       [-]
+        Some(BytesRange::new(Some(0), None)), //       [--------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),    //            [------->
+        BytesRange::new(None, Some(2)),    //         <--]
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),    //            [------->
+        BytesRange::new(Some(0), Some(1)), //          []
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None), //               [------->
+        BytesRange::new(None, Some(1)), //            <-]
+        None,
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(Some(2), None),    //            [------->
+        BytesRange::new(None, None),       //         <---------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+      (
+        //                                             0123456789
+        BytesRange::new(None, None),       //         <---------->
+        BytesRange::new(None, None),       //         <---------->
+        Some(BytesRange::new(None, None)), //         <---------->
+      ),
+    ];
+
+    for (index, (a, b, expected)) in test_cases.iter().enumerate() {
+      println!("Test case {}", index);
+      println!("  {:?}", a);
+      println!("  {:?}", b);
+      println!("  {:?}", expected);
+
+      if a.overlaps(b) {
+        assert_eq!(*a.clone().merge_with(b), expected.clone().unwrap());
+      } else {
+        assert!(expected.is_none())
+      }
+    }
+  }
+
+  #[test]
+  fn bytes_range_merge_all_when_list_is_empty() {
+    assert_eq!(BytesRange::merge_all(Vec::new()), Vec::new());
+  }
+
+  #[test]
+  fn bytes_range_merge_all_when_list_has_one_range() {
+    assert_eq!(
+      BytesRange::merge_all(vec![BytesRange::default()]),
+      vec![BytesRange::default()]
+    );
+  }
+
+  #[test]
+  fn bytes_range_merge_all_when_list_has_many_ranges() {
+    let ranges = vec![
+      BytesRange::new(None, Some(1)),
+      BytesRange::new(Some(1), Some(2)),
+      BytesRange::new(Some(5), Some(6)),
+      BytesRange::new(Some(5), Some(8)),
+      BytesRange::new(Some(6), Some(7)),
+      BytesRange::new(Some(4), Some(5)),
+      BytesRange::new(Some(3), Some(6)),
+      BytesRange::new(Some(10), Some(12)),
+      BytesRange::new(Some(10), Some(12)),
+      BytesRange::new(Some(10), Some(14)),
+      BytesRange::new(Some(14), Some(15)),
+      BytesRange::new(Some(12), Some(16)),
+      BytesRange::new(Some(17), Some(19)),
+      BytesRange::new(Some(21), Some(23)),
+      BytesRange::new(Some(18), Some(22)),
+      BytesRange::new(Some(24), None),
+      BytesRange::new(Some(24), Some(30)),
+      BytesRange::new(Some(31), Some(33)),
+      BytesRange::new(Some(35), None),
+    ];
+
+    let expected_ranges = vec![
+      BytesRange::new(None, Some(2)),
+      BytesRange::new(Some(3), Some(8)),
+      BytesRange::new(Some(10), Some(16)),
+      BytesRange::new(Some(17), Some(23)),
+      BytesRange::new(Some(24), None),
+    ];
+
+    assert_eq!(BytesRange::merge_all(ranges), expected_ranges);
+  }
 }
