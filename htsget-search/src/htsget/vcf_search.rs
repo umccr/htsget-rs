76822c6675936f0397ebc756132a81b351e602b1
Roman Valls Guimera <brainstorm@nopcode.org>
2021-05-21 09:14:36 +1000

    Fmt

--- htsget-search/src/htsget/vcf_search.rs
+++ htsget-search/src/htsget/vcf_search.rs
@@ -1,18 +1,18 @@
 //! Module providing the search capability using VCF files
 //!
 
-use std::str::FromStr;
-use std::{fs::File, path::Path};//, io::{BufReader}};
 use std::convert::TryInto;
+use std::str::FromStr;
+use std::{fs::File, path::Path}; //, io::{BufReader}};
 
-use noodles_core::{Region};
-use noodles_vcf::{self as vcf};
-use noodles_tabix::{self as tabix};
 use noodles_bgzf::{self as bgzf, VirtualPosition};
+use noodles_core::Region;
+use noodles_tabix::{self as tabix};
+use noodles_vcf::{self as vcf};
 
 use crate::{
-    htsget::{Class, Format, HtsGetError, Query, Response, Result, Url},
-    storage::{BytesRange, GetOptions, Storage, UrlOptions},
+  htsget::{Class, Format, HtsGetError, Query, Response, Result, Url},
+  storage::{BytesRange, GetOptions, Storage, UrlOptions},
 };
 
 // TODO: This trait is clearly common across, **at least**, VCF and BAM
@@ -47,102 +47,111 @@ impl VirtualPositionExt for VirtualPosition {
 }
 
 pub(crate) struct VCFSearch<'a, S> {
-    storage: &'a S,
+  storage: &'a S,
 }
 
 impl<'a, S> VCFSearch<'a, S>
 where
   S: Storage + 'a,
 {
-    pub fn new(storage: &'a S) -> Self {
-        Self { storage }
-    }
-   
-    /// TODO: The (VCF)::Reader::query method is mirrored into the BAM implementation, reducing code duplication
-    pub fn search(&self, query: Query) -> Result<Response> {
-      let (vcf_key, _tbi_key) = self.get_keys_from_id(query.id.as_str());
-  
-      match query.class {
-        None | Some(Class::Body) => {
-          let _tbi_path = self.storage.get(&vcf_key, GetOptions::default())?; // TODO: Be more flexible/resilient with index files, do not just assume `.tbi` within the same directory 
-          //let vcf_index = tabix::read(tbi_path).map_err(|_| HtsGetError::io_error("Reading TBI"))?;
-          let (mut vcf_reader, _vcf_header, vcf_index)  = self.read_vcf(&vcf_key)?;
+  pub fn new(storage: &'a S) -> Self {
+    Self { storage }
+  }
 
-          let query_results = match query.reference_name.as_ref() {
-            None => vcf_reader.query(&vcf_index, &Region::All),
-            Some(reference_name) if reference_name.as_str() == "*" => {
-              vcf_reader.query(&vcf_index, &Region::Unmapped)
-            }
-            Some(reference_name) => vcf_reader.query(&vcf_index, &Region::from_str(reference_name.as_str())?)              
-          }?;
+  /// TODO: The (VCF)::Reader::query method is mirrored into the BAM implementation, reducing code duplication
+  pub fn search(&self, query: Query) -> Result<Response> {
+    let (vcf_key, _tbi_key) = self.get_keys_from_id(query.id.as_str());
 
-          // let br = BytesRange::new(query_results.unwrap().start.try_into().unwrap(),
-          //                          query_results.unwrap().end.try_into().unwrap());
+    match query.class {
+      None | Some(Class::Body) => {
+        let _tbi_path = self.storage.get(&vcf_key, GetOptions::default())?; // TODO: Be more flexible/resilient with index files, do not just assume `.tbi` within the same directory
+                                                                            //let vcf_index = tabix::read(tbi_path).map_err(|_| HtsGetError::io_error("Reading TBI"))?;
+        let (mut vcf_reader, _vcf_header, vcf_index) = self.read_vcf(&vcf_key)?;
 
-          let start_result = query_results.start.try_into().unwrap();
-          let end_result = query_results.end.try_into().unwrap();
+        let query_results = match query.reference_name.as_ref() {
+          None => vcf_reader.query(&vcf_index, &Region::All),
+          Some(reference_name) if reference_name.as_str() == "*" => {
+            vcf_reader.query(&vcf_index, &Region::Unmapped)
+          }
+          Some(reference_name) => {
+            vcf_reader.query(&vcf_index, &Region::from_str(reference_name.as_str())?)
+          }
+        }?;
 
-          let br = BytesRange::default().with_start(start_result)
-                                        .with_end(end_result);
+        // let br = BytesRange::new(query_results.unwrap().start.try_into().unwrap(),
+        //                          query_results.unwrap().end.try_into().unwrap());
 
-          let vbr = vec!(br);
-          self.build_response(query, &vcf_key, vbr)
-        }
-        Some(Class::Header) => {
-          todo!()
-          // let byte_ranges = todo!();
-          // self.build_response(query, &vcf_key, byte_ranges.into_iter().collect())
-        }
+        let start_result = query_results.start.try_into().unwrap();
+        let end_result = query_results.end.try_into().unwrap();
+
+        let br = BytesRange::default()
+          .with_start(start_result)
+          .with_end(end_result);
+
+        let vbr = vec![br];
+        self.build_response(query, &vcf_key, vbr)
+      }
+      Some(Class::Header) => {
+        todo!()
+        // let byte_ranges = todo!();
+        // self.build_response(query, &vcf_key, byte_ranges.into_iter().collect())
       }
     }
+  }
 
-    fn get_keys_from_id(&self, id: &str) -> (String, String) {
-      let vcf_key = format!("{}.vcf.gz", id); // TODO: allow uncompressed, plain, .vcf files
-      let tbi_key = format!("{}.vcf.gz.tbi", id);
-      (vcf_key, tbi_key)
-    }
+  fn get_keys_from_id(&self, id: &str) -> (String, String) {
+    let vcf_key = format!("{}.vcf.gz", id); // TODO: allow uncompressed, plain, .vcf files
+    let tbi_key = format!("{}.vcf.gz.tbi", id);
+    (vcf_key, tbi_key)
+  }
 
-    fn read_vcf<P: AsRef<Path>>(&self, path: P) -> Result<(vcf::Reader<noodles_bgzf::Reader<std::fs::File>>, vcf::Header, tabix::Index)> {
-      let mut vcf_reader = File::open(&path)
-        .map(bgzf::Reader::new) 
-        .map(vcf::Reader::new)
-        .map_err(|_| HtsGetError::io_error("Reading VCF"))?;
- 
-      let vcf_header = vcf_reader
-        .read_header()
-        .map_err(|_| HtsGetError::io_error("Reading VCF header"))?
-        .parse()
-        .map_err(|_| HtsGetError::io_error("Parsing VCF header"))?;
-      
-     let vcf_index = tabix::read(&path)
-        .map_err(|_| HtsGetError::io_error("Reading index"))?; //+".tbi" is typical vcf index extension, but should be flexible accepting other fnames
-      
-      Ok((vcf_reader, vcf_header, vcf_index))
-    }
+  fn read_vcf<P: AsRef<Path>>(
+    &self,
+    path: P,
+  ) -> Result<(
+    vcf::Reader<noodles_bgzf::Reader<std::fs::File>>,
+    vcf::Header,
+    tabix::Index,
+  )> {
+    let mut vcf_reader = File::open(&path)
+      .map(bgzf::Reader::new)
+      .map(vcf::Reader::new)
+      .map_err(|_| HtsGetError::io_error("Reading VCF"))?;
 
-    fn build_response(
-      &self,
-      query: Query,
-      vcf_key: &str,
-      byte_ranges: Vec<BytesRange>,
-    ) -> Result<Response> {
-      let urls = byte_ranges
-        .into_iter()
-        .map(|range| {
-          let options = match query.class.as_ref() {
-            None => UrlOptions::default().with_range(range),
-            Some(class) => UrlOptions::default()
-              .with_range(range)
-              .with_class(class.clone()),
-          };
-          self
-            .storage
-            .url(&vcf_key, options)
-            .map_err(HtsGetError::from)
-        })
-        .collect::<Result<Vec<Url>>>()?;
+    let vcf_header = vcf_reader
+      .read_header()
+      .map_err(|_| HtsGetError::io_error("Reading VCF header"))?
+      .parse()
+      .map_err(|_| HtsGetError::io_error("Parsing VCF header"))?;
 
-      let format = query.format.unwrap_or(Format::Vcf);
-      Ok(Response::new(format, urls))
-    }
+    let vcf_index = tabix::read(&path).map_err(|_| HtsGetError::io_error("Reading index"))?; //+".tbi" is typical vcf index extension, but should be flexible accepting other fnames
+
+    Ok((vcf_reader, vcf_header, vcf_index))
+  }
+
+  fn build_response(
+    &self,
+    query: Query,
+    vcf_key: &str,
+    byte_ranges: Vec<BytesRange>,
+  ) -> Result<Response> {
+    let urls = byte_ranges
+      .into_iter()
+      .map(|range| {
+        let options = match query.class.as_ref() {
+          None => UrlOptions::default().with_range(range),
+          Some(class) => UrlOptions::default()
+            .with_range(range)
+            .with_class(class.clone()),
+        };
+        self
+          .storage
+          .url(&vcf_key, options)
+          .map_err(HtsGetError::from)
+      })
+      .collect::<Result<Vec<Url>>>()?;
+
+    let format = query.format.unwrap_or(Format::Vcf);
+    Ok(Response::new(format, urls))
+  }
 }
