827a40799dbfef37d0d30dd98b49668f58bc4803
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-07-15 13:35:00 +0100

    Add the variants endpoint

--- htsget-http-actix/src/main.rs
+++ htsget-http-actix/src/main.rs
@@ -16,7 +16,32 @@ async fn reads(
 ) -> impl Responder {
   let mut query_information = query.into_inner();
   query_information.insert("id".to_string(), id);
-  let response = get_response(shared_state.get_ref(), &query_information);
+  if let None = query_information.get("format") {
+    query_information.insert("format".to_string(), "BAM".to_string());
+  }
+  handle_request(query_information, shared_state.get_ref())
+}
+
+// TODO: Don't accept the variants formats in the reads endpoint and viceversa
+#[get("/variants/{id:.+}")]
+async fn variants(
+  query: Query<HashMap<String, String>>,
+  Path(id): Path<String>,
+  shared_state: Data<HtsGetFromStorage<LocalStorage>>,
+) -> impl Responder {
+  let mut query_information = query.into_inner();
+  query_information.insert("id".to_string(), id);
+  if let None = query_information.get("format") {
+    query_information.insert("format".to_string(), "VCF".to_string());
+  }
+  handle_request(query_information, shared_state.get_ref())
+}
+
+fn handle_request(
+  query_information: HashMap<String, String>,
+  htsget: &HtsGetFromStorage<LocalStorage>,
+) -> impl Responder {
+  let response = get_response(htsget, &query_information);
   match response {
     Err(error) => {
       let (json, status_code) = error.to_json_representation();
@@ -32,7 +57,7 @@ async fn main() -> std::io::Result<()> {
     let htsget = HtsGetFromStorage::new(
       LocalStorage::new("data").expect("Couldn't create a Storage with the provided path"),
     );
-    App::new().data(htsget).service(reads)
+    App::new().data(htsget).service(reads).service(variants)
   })
   .bind("127.0.0.1:8080")?
   .run()
