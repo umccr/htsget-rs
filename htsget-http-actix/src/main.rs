ac0281922da97e41d6846d7a2c5b9b8ca3198f96
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-07-26 10:46:19 +0100

    Use envy instead of command line arguments

--- htsget-http-actix/src/main.rs
+++ htsget-http-actix/src/main.rs
@@ -1,57 +1,39 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web::Data, App, HttpServer};
 use htsget_search::{htsget::from_storage::HtsGetFromStorage, storage::local::LocalStorage};
 use std::env::args;
+mod config;
+use config::Config;
 mod get;
 mod post;
 
-const USAGE: &str =
-  "Usage: [-p|--port {port number}] [-i|--ip {ip address}]\nThe default address is 127.0.0.1:8080";
-const DEFAULT_IP: &str = "127.0.0.1";
-const DEFAULT_PORT: &str = "8080";
+const USAGE: &str = r"
+There are some environment variables that can be set to configure the server:
+* HTSGET_IP: The ip to use. Default: 127.0.0.1
+* HTSGET_PORT: The port to use. Default: 8080
+* HTSGET_PATH: The path to the directory where the server should be started. Default: Actual directory
+";
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  let address = match parse_args() {
-    Some(result) => result,
-    None => {
-      println!("{}", USAGE);
-      return Ok(());
-    }
-  };
-  HttpServer::new(|| {
-    let htsget = HtsGetFromStorage::new(
-      LocalStorage::new("data").expect("Couldn't create a Storage with the provided path"),
-    );
+  if args().any(|arg| arg == "-h" || arg == "--help") {
+    println!("{}", USAGE);
+    return Ok(());
+  }
+  let config = envy::from_env::<Config>().expect("The environment variables weren't properly set!");
+  let htsget = HtsGetFromStorage::new(
+    LocalStorage::new(config.htsget_path)
+      .expect("Couldn't create a Storage with the provided path"),
+  );
+  let htsget = Data::new(htsget);
+  HttpServer::new(move || {
     App::new()
-      .data(htsget)
+      .app_data(htsget.clone())
       .service(get::reads)
       .service(get::variants)
       .service(post::reads)
       .service(post::variants)
   })
-  .bind(address)?
+  .bind(format!("{}:{}", config.htsget_ip, config.htsget_port))?
   .run()
   .await
-}
-
-fn parse_args() -> Option<String> {
-  let args: Vec<String> = args().skip(1).collect();
-  let mut ip = DEFAULT_IP.to_string();
-  let mut port = DEFAULT_PORT.to_string();
-  let mut counter = 0;
-  while counter < args.len() {
-    match args[counter].as_str() {
-      "-p" | "--port" => {
-        counter += 1;
-        port = args.get(counter)?.clone();
-      }
-      "-i" | "--ip" => {
-        counter += 1;
-        ip = args.get(counter)?.clone();
-      }
-      _ => return None,
-    }
-    counter += 1;
-  }
-  Some(format!("{}:{}", ip, port))
 }
