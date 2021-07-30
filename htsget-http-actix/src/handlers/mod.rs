887412386de90cc00174706288fc411b517b4417
CastilloDel <delcastillodelarosadaniel@gmail.com>
2021-07-30 13:07:22 +0100

    Actually use the new configuration variables

--- htsget-http-actix/src/handlers/mod.rs
+++ htsget-http-actix/src/handlers/mod.rs
@@ -48,6 +48,21 @@ fn fill_out_service_info_json(mut service_info_json: ServiceInfo, config: &Confi
   if let Some(organization_url) = &config.htsget_organization_url {
     service_info_json.organization.url = organization_url.clone();
   }
+  if let Some(contact_url) = &config.htsget_contact_url {
+    service_info_json.contact_url = contact_url.clone();
+  }
+  if let Some(documentation_url) = &config.htsget_documentation_url {
+    service_info_json.documentation_url = documentation_url.clone();
+  }
+  if let Some(created_at) = &config.htsget_created_at {
+    service_info_json.created_at = created_at.clone();
+  }
+  if let Some(updated_at) = &config.htsget_updated_at {
+    service_info_json.updated_at = updated_at.clone();
+  }
+  if let Some(environment) = &config.htsget_environment {
+    service_info_json.environment = environment.clone();
+  }
   service_info_json
 }
 
