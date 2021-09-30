4ee9b6e0df7e7149d8ff3d955e4474a147188831
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-09-30 14:27:43 +1000

    AwsS3Storage struct does not need url/presigned url since it's generated a posteriori. No crates for s3 url parsing, just two mandatory traits left to implement: url and head

--- htsget-search/src/storage/aws.rs
+++ htsget-search/src/storage/aws.rs
@@ -1,5 +1,4 @@
 //! Module providing an implementation for the [Storage] trait using Amazon's S3 object storage service.
-//!
 use std::path::PathBuf;
 
 use async_trait::async_trait;
@@ -10,11 +9,13 @@ use super::{GetOptions, Result, UrlOptions};
 use crate::storage::async_storage::AsyncStorage;
 //#[cfg(feature = "aws_rust_sdk")]
 //use aws_sdk_s3 as s3;
+
 //#[cfg(feature = "rusoto")]
 use rusoto_core::{
   credential::{DefaultCredentialsProvider, ProvideAwsCredentials},
   Region,
 };
+
 use rusoto_s3 as s3;
 use rusoto_s3::util::PreSignedRequest;
 
@@ -22,18 +23,18 @@ use rusoto_s3::util::PreSignedRequest;
 //use super::Result;
 
 // TODO: Use envy for AWS creds?
-// TODO: Encode object "reachability" in this enum?
 enum Reachability {
   ImmediateRetrieval,
   DelayedRetrieval,
 }
 
-enum AwsStorageTier {
+// TODO: Encode object "reachability" more statically in this enum?
+enum AwsS3StorageTier {
   Standard(Reachability),
   StandardIa(Reachability),
   OnezoneIa(Reachability),
-  Glacier(Reachability),      // ~24-48 hours
-  DeepArchive(Reachability),  // ~48 hours
+  Glacier(Reachability),     // ~24-48 hours
+  DeepArchive(Reachability), // ~48 hours
 }
 
 /// Implementation for the [Storage] trait using the local file system.
@@ -41,26 +42,19 @@ pub struct AwsS3Storage {
   bucket: String,
   key: String,
   region: Region,
-  presigned_url: String,
-  tier: AwsStorageTier,
+  tier: AwsS3StorageTier,
 }
 
 impl AwsS3Storage {
-  fn new(
-    bucket: String,
-    key: String,
-    region: Region,
-    presigned_url: String,
-    tier: AwsStorageTier,
-  ) -> Self {
+  fn new(bucket: String, key: String, region: Region, tier: AwsS3StorageTier) -> Self {
     AwsS3Storage {
       bucket,
       key,
       region,
-      presigned_url,
-      tier: tier,
+      tier,
     }
   }
+
   // TODO: infer region?: https://rusoto.github.io/rusoto/rusoto_s3/struct.GetBucketLocationRequest.html
   fn get_region(&self) -> Region {
     let region = if let Ok(url) = std::env::var("AWS_ENDPOINT_URL") {
@@ -75,16 +69,17 @@ impl AwsS3Storage {
     region
   }
 
-  async fn _get_bucket_and_key_from_s3_url(&self, _s3_url: String) -> Result<(String, String)> {
-    unimplemented!();
-    // https://gist.github.com/bh1428/c30b7db493828ece622a6cb38c05362a
+  // TODO: Take into account all S3 URL styles...: https://gist.github.com/bh1428/c30b7db493828ece622a6cb38c05362a
+  async fn get_bucket_and_key_from_s3_url(s3_url: String) -> Result<(String, String)> {
+    let parts: Vec<&str> = s3_url.split_terminator("/").collect();
+    Ok((parts[2].to_string(), parts[3].to_string()))
   }
 
-  async fn s3_presign_url(bucket_name: String, key: String) -> String {
+  async fn s3_presign_url(bucket: String, key: String) -> Result<String> {
     let region = Region::ApSoutheast2;
     let req = s3::GetObjectRequest {
-      bucket: bucket_name,
-      key: key,
+      bucket,
+      key,
       ..Default::default()
     };
     let credentials = DefaultCredentialsProvider::new()
@@ -93,10 +88,12 @@ impl AwsS3Storage {
       .await
       .unwrap();
     //PreSignedRequestOption expires_in: 3600
-    req.get_presigned_url(&region, &credentials, &Default::default())
+    Ok(req.get_presigned_url(&region, &credentials, &Default::default()))
   }
 
-  async fn determine_reachability() -> Result<AwsStorageTier> {
+  async fn determine_reachability() -> Result<AwsS3StorageTier> {
+    // 1. S3 head request to object
+    // 2. Return status
     unimplemented!();
   }
 }
@@ -104,13 +101,18 @@ impl AwsS3Storage {
 #[async_trait]
 impl AsyncStorage for AwsS3Storage {
   async fn get<K: AsRef<str> + Send>(&self, key: K, _options: GetOptions) -> Result<PathBuf> {
-    //let (bucket, s3key) = self.get_bucket_and_key_from_s3_url(key).await?;
-    unimplemented!();
-    // Ok(PathBuf::from(key))
+    let key: &str = key.as_ref();
+    let (bucket, s3key) = AwsS3Storage::get_bucket_and_key_from_s3_url(key.to_string()).await?;
+
+    let s3path = PathBuf::from(bucket).join(s3key);
+
+    Ok(s3path)
   }
 
   async fn url<K: AsRef<str> + Send>(&self, key: K, options: UrlOptions) -> Result<Url> {
     unimplemented!();
+    // let presigned_url = AwsS3Storage::s3_presign_url(self.bucket.clone(), key.as_ref().to_string()).await;
+    // Ok(presigned_url)
   }
 
   async fn head<K: AsRef<str> + Send>(&self, key: K) -> Result<u64> {
@@ -126,7 +128,18 @@ mod tests {
   async fn split_s3_url_into_bucket_and_key() {
     let s3_url = "s3://bucket/key";
 
-    let (bucket, key) = AwsS3Storage::new(&self, s3_url).get_bucket_and_key_from_s3_url(s3_url)?;
+    // TODO: This method should be outside of AwsS3Storage impl because bucket & key are needed prior to ::new()... utils?
+    let (bucket, key) = AwsS3Storage::get_bucket_and_key_from_s3_url(s3_url.to_string())
+      .await
+      .unwrap();
+
+    let s3_storage = AwsS3Storage::new(
+      bucket.clone(),
+      key.clone(),
+      Region::ApSoutheast2,
+      AwsS3StorageTier::Standard(Reachability::ImmediateRetrieval),
+    );
+
     assert_eq!(bucket, "bucket");
     assert_eq!(key, "key");
   }
