21072ba76a8b01bcc36a87b685c2e41c06649d64
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-10-13 16:36:25 +1100

    Tests re-enabled and migrated to aws_sdk_rust and region/tier-less struct. The head bytes test fails differently, need to see if Custom regions are allowed in aws_sdk_rust as they were in Rusoto and/or come up with a different strategy when testing head()

--- htsget-search/src/storage/aws.rs
+++ htsget-search/src/storage/aws.rs
@@ -36,19 +36,17 @@ enum AwsS3StorageTier {
 
 /// Implementation for the [Storage] trait using the local file system.
 pub struct AwsS3Storage {
+  client: Client,
   bucket: String,
   key: String,
-  region: Region,
-  tier: AwsS3StorageTier,
 }
 
 impl AwsS3Storage {
-  fn new(bucket: String, key: String, region: Region, tier: AwsS3StorageTier) -> Self {
+  fn new(client: Client, bucket: String, key: String) -> Self {
     AwsS3Storage {
+      client,
       bucket,
       key,
-      region,
-      tier,
     }
   }
 
@@ -150,8 +148,8 @@ impl AsyncStorage for AwsS3Storage {
   /// Returns a S3-presigned htsget URL
   async fn url<K: AsRef<str> + Send>(&self, key: K, options: UrlOptions) -> Result<Url> {
     let region_provider = RegionProviderChain::first_try("ap-southeast-2")
-    .or_default_provider()
-    .or_else(Region::new("us-east-1"));
+      .or_default_provider()
+      .or_else(Region::new("us-east-1"));
   
     let shared_config = aws_config::from_env().region(region_provider).load().await;
     
@@ -182,136 +180,145 @@ impl AsyncStorage for AwsS3Storage {
   }
 }
 
-// #[cfg(test)]
-// mod tests {
-//   use super::*;
+#[cfg(test)]
+mod tests {
+  use super::*;
 
-//   use crate::storage::s3_testing::setup_service;
-//   use crate::storage::s3_testing::recv_body_string;
+  use crate::storage::s3_testing::setup_service;
+  use crate::storage::s3_testing::recv_body_string;
+  use crate::storage::s3_testing::fs_write_object;
 
-//   use hyper::{Body, Method, StatusCode};
-//   use s3_server::headers::HeaderValue;
-//   use s3_server::headers::X_AMZ_CONTENT_SHA256;
+  use hyper::{Body, Method, StatusCode};
+  use s3_server::headers::HeaderValue;
+  use s3_server::headers::X_AMZ_CONTENT_SHA256;
 
-//   type Request = hyper::Request<hyper::Body>;
+  type Request = hyper::Request<hyper::Body>;
 
-//   #[tokio::test]
-//   async fn test_split_s3_url_into_bucket_and_key() {
-//     let s3_url = "s3://bucket/key";
+  async fn aws_s3_client() -> Client {
+    let region_provider = RegionProviderChain::first_try("ap-southeast-2")
+    .or_default_provider()
+    .or_else(Region::new("us-east-1"));
 
-//     let (bucket, key) = AwsS3Storage::get_bucket_and_key_from_s3_url(s3_url.to_string())
-//       .await
-//       .unwrap();
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+  
+    Client::new(&shared_config)
+  }
 
-//     let s3_storage = AwsS3Storage::new(
-//       bucket.clone(),
-//       key.clone(),
-//       Region::ApSoutheast2,
-//       AwsS3StorageTier::Standard(Retrieval::Immediate),
-//     );
+  #[tokio::test]
+  async fn test_split_s3_url_into_bucket_and_key() {
+    let s3_url = "s3://bucket/key";
 
-//     assert_eq!(bucket, "bucket");
-//     assert_eq!(key, "key");
-//   }
+    let (bucket, key) = AwsS3Storage::get_bucket_and_key_from_s3_url(s3_url.to_string())
+      .await
+      .unwrap();
+      
+    let s3_storage = AwsS3Storage::new(
+      aws_s3_client().await,
+      bucket.clone(),
+      key.clone(),
+    );
 
-//   #[tokio::test]
-//   async fn test_get_htsget_url_from_s3() {
-//     let s3_url = "s3://bucket/key";
+    assert_eq!(bucket, "bucket");
+    assert_eq!(key, "key");
+  }
 
-//     let (bucket, key) = AwsS3Storage::get_bucket_and_key_from_s3_url(s3_url.to_string())
-//       .await
-//       .unwrap();
+  #[tokio::test]
+  async fn test_get_htsget_url_from_s3() {
+    let s3_url = "s3://bucket/key";
 
-//     let s3_storage = AwsS3Storage::new(
-//       bucket.clone(),
-//       key.clone(),
-//       Region::ApSoutheast2,
-//       AwsS3StorageTier::Standard(Retrieval::Immediate),
-//     );
+    let (bucket, key) = AwsS3Storage::get_bucket_and_key_from_s3_url(s3_url.to_string())
+      .await
+      .unwrap();
 
-//     let htsget_url = s3_storage.url(key, UrlOptions::default()).await.unwrap();
-//     //dbg!(&htsget_url);
-//     assert!(htsget_url.url.contains("X-Amz-Signature"));
-//   }
+    let s3_storage = AwsS3Storage::new(
+      aws_s3_client().await,
+      bucket.clone(),
+      key.clone(),
+    );
 
-//   #[tokio::test]
-//   async fn test_get_head_bytes_from_s3() {
-//     // Tilt up the local S3 server...
-//     let (root, service) = setup_service().unwrap();
+    let htsget_url = s3_storage.url(key, UrlOptions::default()).await.unwrap();
+    //dbg!(&htsget_url);
+    assert!(htsget_url.url.contains("X-Amz-Signature"));
+  }
+
+  #[tokio::test]
+  async fn test_get_head_bytes_from_s3() {
+    // Tilt up the local S3 server...
+    let (root, service) = setup_service().unwrap();
  
-//     let bucket = "asd";
-//     let key = "qwe";
-//     let content = "Hello World!";
+    let bucket = "asd";
+    let key = "qwe";
+    let content = "Hello World!";
 
-//     fs_write_object(root, bucket, key, content).unwrap();
+    fs_write_object(root, bucket, key, content).unwrap();
 
-//     let mut req = Request::new(Body::empty());
-//     *req.method_mut() = Method::GET;
-//     *req.uri_mut() = format!("http://localhost:8014/{}/{}", bucket, key)
-//         .parse()
-//         .unwrap();
-//     req.headers_mut().insert(
-//         X_AMZ_CONTENT_SHA256.clone(),
-//         HeaderValue::from_static("UNSIGNED-PAYLOAD"),
-//     );
+    let mut req = Request::new(Body::empty());
+    *req.method_mut() = Method::GET;
+    *req.uri_mut() = format!("http://localhost:8014/{}/{}", bucket, key)
+        .parse()
+        .unwrap();
+    req.headers_mut().insert(
+        X_AMZ_CONTENT_SHA256.clone(),
+        HeaderValue::from_static("UNSIGNED-PAYLOAD"),
+    );
 
-//     let mut res = service.hyper_call(req).await.unwrap();
-//     let body = recv_body_string(&mut res).await.unwrap();
+    let mut res = service.hyper_call(req).await.unwrap();
+    let body = recv_body_string(&mut res).await.unwrap();
 
-//     let local_region = Region::Custom {
-//       endpoint: "http://localhost:8014".to_owned(),
-//       name: "local".to_owned(),
-//     };
+    // TODO: Find an aws_sdk_rust equivalent? Not sure this exists :_S
+    // let local_region = Region::Custom {
+    //   endpoint: "http://localhost:8014".to_owned(),
+    //   name: "local".to_owned(),
+    // };
 
-//     let s3_storage = AwsS3Storage::new(
-//       bucket.to_string(),
-//       key.to_string(),
-//       local_region,
-//       AwsS3StorageTier::Standard(Retrieval::Immediate),
-//     );
+    let s3_storage = AwsS3Storage::new(
+      aws_s3_client().await,
+      bucket.to_string(),
+      key.to_string(),
+    );
 
-//     let obj_head = format!("http://localhost:8014/{}/{}", bucket, key);
-//     dbg!(&obj_head);
-//     let htsget_head = s3_storage.head(obj_head).await.unwrap();
+    let obj_head = format!("http://localhost:8014/{}/{}", bucket, key);
+    dbg!(&obj_head);
+    let htsget_head = s3_storage.head(obj_head).await.unwrap();
 
-//     assert_eq!(res.status(), StatusCode::OK);
-//     // assert_eq!(body, content);
-//   }
+    // assert_eq!(res.status(), StatusCode::OK);
+    // assert_eq!(body, content);
+  }
 
-//   #[tokio::test]
-//   async fn test_get_local_s3_server_object() {
-//       let (root, service) = setup_service().unwrap();
+  #[tokio::test]
+  async fn test_get_local_s3_server_object() {
+      let (root, service) = setup_service().unwrap();
 
-//       let bucket = "asd";
-//       let key = "qwe";
-//       let content = "Hello World!";
+      let bucket = "asd";
+      let key = "qwe";
+      let content = "Hello World!";
 
-//       fs_write_object(root, bucket, key, content).unwrap();
+      fs_write_object(root, bucket, key, content).unwrap();
 
-//       let mut req = Request::new(Body::empty());
-//       *req.method_mut() = Method::GET;
-//       *req.uri_mut() = format!("http://localhost:8014/{}/{}", bucket, key)
-//           .parse()
-//           .unwrap();
-//       req.headers_mut().insert(
-//           X_AMZ_CONTENT_SHA256.clone(),
-//           HeaderValue::from_static("UNSIGNED-PAYLOAD"),
-//       );
+      let mut req = Request::new(Body::empty());
+      *req.method_mut() = Method::GET;
+      *req.uri_mut() = format!("http://localhost:8014/{}/{}", bucket, key)
+          .parse()
+          .unwrap();
+      req.headers_mut().insert(
+          X_AMZ_CONTENT_SHA256.clone(),
+          HeaderValue::from_static("UNSIGNED-PAYLOAD"),
+      );
 
-//       let mut res = service.hyper_call(req).await.unwrap();
-//       let body = recv_body_string(&mut res).await.unwrap();
+      let mut res = service.hyper_call(req).await.unwrap();
+      let body = recv_body_string(&mut res).await.unwrap();
 
-//       assert_eq!(res.status(), StatusCode::OK);
-//       assert_eq!(body, content);
-//   }
+      assert_eq!(res.status(), StatusCode::OK);
+      assert_eq!(body, content);
+  }
 
-//   // #[tokio::test]
-//   // async fn local_s3_server_returns_htsget_url() {
-//   //   let (root, service) = setup_service().unwrap();
+  #[tokio::test]
+  async fn local_s3_server_returns_htsget_url() {
+    let (root, service) = setup_service().unwrap();
 
-//   //   let bucket = "bucket";
-//   //   let key = "key";
-//   //   let content = "Hello World!";
+    let bucket = "bucket";
+    let key = "key";
+    let content = "Hello World!";
 
-//   // }
-// }
+  }
+}
