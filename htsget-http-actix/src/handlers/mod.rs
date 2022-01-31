8d42d2f5c62c6915c54ca51d85248b018b25d7da
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-01-31 16:00:29 +1100

    Catch up with actix-web, next up is noodles

--- htsget-http-actix/src/handlers/mod.rs
+++ htsget-http-actix/src/handlers/mod.rs
@@ -25,9 +25,9 @@ fn handle_response(response: Result<JsonResponse>) -> Either<impl Responder, imp
   match response {
     Err(error) => {
       let (json, status_code) = error.to_json_representation();
-      Either::Left(PrettyJson(json).with_status(StatusCode::from_u16(status_code).unwrap()))
+      Either::Left(PrettyJson(json).customize().with_status(StatusCode::from_u16(status_code).unwrap()))
     }
-    Ok(json) => Either::Right(PrettyJson(json).with_status(StatusCode::OK)),
+    Ok(json) => Either::Right(PrettyJson(json).customize().with_status(StatusCode::OK)),
   }
 }
 
