d6ad1d0cac4863c5af9598856fbb30b955a6eaad
Marko Malenic <mmalenic1@gmail.com>
2022-04-21 09:26:01 +1000

    Local server works.

--- htsget-search/src/storage/local_server.rs
+++ htsget-search/src/storage/local_server.rs
@@ -1,6 +1,7 @@
 use std::net::{AddrParseError, SocketAddr, TcpListener};
 use std::path::Path;
 use axum::{Error, Router};
+use axum::routing::get;
 use axum_extra::routing::SpaRouter;
 use tokio::task::JoinHandle;
 use crate::storage::{Storage, UrlFormatter};
@@ -52,10 +53,51 @@ impl UrlFormatter for LocalStorageServer {
   }
 
   fn format_scheme(&self) -> String {
-    axum::http::uri::Scheme::HTTPS.to_string()
+    axum::http::uri::Scheme::HTTP.to_string()
   }
 
   fn format_authority(&self) -> String {
     format!("{}:{}", self.ip, self.port)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use std::future::Future;
+  use std::{matches, time};
+  use std::net::{SocketAddr, TcpListener};
+  use std::thread::sleep;
+  use axum::routing::get;
+  use http::Request;
+  use hyper::Body;
+
+  use tempfile::TempDir;
+  use tokio::fs::{create_dir, File};
+  use tokio::io::AsyncWriteExt;
+
+  use crate::htsget::{Headers, Url};
+  use crate::storage::{BytesRange, GetOptions, StorageError, UrlOptions};
+  use crate::storage::local::tests::create_local_test_files;
+
+  use super::*;
+
+  #[tokio::test]
+  async fn test_start_server() {
+    let (_, base_path) = create_local_test_files().await;
+
+    LocalStorageServer::new("127.0.0.1", "8080").start_server(base_path.path()).unwrap();
+
+    let client = hyper::Client::new();
+    let request = Request::builder()
+      .uri(format!("http://{}:{}/data/key1", "127.0.0.1", "8080"))
+      .body(Body::empty())
+      .unwrap();
+    let response = client
+      .request(request)
+      .await
+      .unwrap();
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(body.as_ref(), b"value1");
   }
 }
