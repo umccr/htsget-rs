7ea605ee8d77ae1ef0b739fade58709ac0df0729
Marko Malenic <mmalenic1@gmail.com>
2022-05-04 15:13:55 +1000

    Update usage string and add a few tests.

--- htsget-config/src/config.rs
+++ htsget-config/src/config.rs
@@ -8,23 +8,27 @@ use serde::Deserialize;
 
 pub const USAGE: &str = r#"
 This executable doesn't use command line arguments, but there are some environment variables that can be set to configure the HtsGet server:
-* HTSGET_IP: The ip to use. Default: 127.0.0.1
-* HTSGET_PORT: The port to use. Default: 8080
-* HTSGET_PATH: The path to the directory where the server should be started. Default: Actual directory
-* HTSGET_REGEX: The regular expression that should match an ID. Default: ".*"
-* HTSGET_REPLACEMENT: The replacement expression. Default: "$0"
+* HTSGET_ADDR: The socket address to use for the server which creates response tickets. Default: "127.0.0.1:8080".
+* HTSGET_PATH: The path to the directory where the server should be started. Default: "."
+* HTSGET_REGEX: The regular expression that should match an ID. Default: ".*".
+* HTSGET_SUBSTITUTION_STRING: The replacement expression. Default: "$0".
 For more information about the regex options look in the documentation of the regex crate(https://docs.rs/regex/).
-The next variables are used to configure the info for the service-info endpoints
-* HTSGET_ID: The id of the service. Default: ""
-* HTSGET_NAME: The name of the service. Default: "HtsGet service"
-* HTSGET_VERSION: The version of the service. Default: ""
-* HTSGET_ORGANIZATION_NAME: The name of the organization. Default: "Snake oil"
-* HTSGET_ORGANIZATION_URL: The url of the organization. Default: "https://en.wikipedia.org/wiki/Snake_oil"
-* HTSGET_CONTACT_URL: A url to provide contact to the users. Default: "",
-* HTSGET_DOCUMENTATION_URL: A link to the documentation. Default: "https://github.com/umccr/htsget-rs/tree/main/htsget-http-actix",
-* HTSGET_CREATED_AT: Date of the creation of the service. Default: "",
-* HTSGET_UPDATED_AT: Date of the last update of the service. Default: "",
-* HTSGET_ENVIRONMENT: The environment in which the service is running. Default: "Testing",
+The next variables are used to configure the info for the service-info endpoints.
+* HTSGET_ID: The id of the service. Default: "None".
+* HTSGET_NAME: The name of the service. Default: "None".
+* HTSGET_VERSION: The version of the service. Default: "None".
+* HTSGET_ORGANIZATION_NAME: The name of the organization. Default: "None".
+* HTSGET_ORGANIZATION_URL: The url of the organization. Default: "None".
+* HTSGET_CONTACT_URL: A url to provide contact to the users. Default: "None".
+* HTSGET_DOCUMENTATION_URL: A link to the documentation. Default: "None".
+* HTSGET_CREATED_AT: Date of the creation of the service. Default: "None".
+* HTSGET_UPDATED_AT: Date of the last update of the service. Default: "None".
+* HTSGET_ENVIRONMENT: The environment in which the service is running. Default: "None".
+* HTSGET_STORAGE_TYPE: Either LocalStorage or AwsS3Storage. Default: "LocalStorage".
+* HTSGET_TICKET_SERVER_ADDR: The socket address to use for the server which responds to tickets. Default: "127.0.0.1:8081". Unused if HTSGET_STORAGE_TYPE is not "LocalStorage".
+* HTSGET_TICKET_SERVER_KEY: The path to the PEM formatted X.509 private key used by the ticket response server. Default: "${HTSGET_PATH}/key.pem". Unused if HTSGET_STORAGE_TYPE is not "LocalStorage".
+* HTSGET_TICKET_SERVER_CERT: The path to the PEM formatted X.509 certificate used by the ticket response server. Default: "${HTSGET_PATH}/cert.pem". Unused if HTSGET_STORAGE_TYPE is not "LocalStorage".
+* HTSGET_S3_BUCKET: The name of the AWS S3 bucket. Default: None. Unused if HTSGET_STORAGE_TYPE is not "AwsS3Storage". Must be specified if using AwsS3Storage.
 "#;
 
 fn default_localstorage_addr() -> SocketAddr {
@@ -39,20 +43,16 @@ fn default_path() -> PathBuf {
   PathBuf::from(".")
 }
 
-fn default_resolver() -> RegexResolver {
-  RegexResolver::new(".*", "$0").expect("Expected valid resolver.")
-}
-
 fn default_localstorage_cert() -> PathBuf {
-  default_path().join("certs/cert.pem")
+  default_path().join("cert.pem")
 }
 
 fn default_localstorage_key() -> PathBuf {
-  default_path().join("certs/key.pem")
+  default_path().join("key.pem")
 }
 
 /// Specify the storage type to use.
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, PartialEq)]
 pub enum StorageType {
   LocalStorage,
   #[cfg(feature = "s3-storage")]
@@ -64,19 +64,21 @@ pub enum StorageType {
 #[serde(default)]
 pub struct Config {
   pub htsget_addr: SocketAddr,
+  #[serde(flatten)]
   pub htsget_resolver: RegexResolver,
   pub htsget_path: PathBuf,
   #[serde(flatten)]
   pub htsget_config_service_info: ConfigServiceInfo,
-  pub htsget_localstorage_addr: SocketAddr,
-  pub htsget_localstorage_cert: PathBuf,
-  pub htsget_localstorage_key: PathBuf,
   pub htsget_storage_type: StorageType,
+  pub htsget_localstorage_addr: SocketAddr,
+  pub htsget_localstorage_key: PathBuf,
+  pub htsget_localstorage_cert: PathBuf,
   #[cfg(feature = "s3-storage")]
   pub htsget_s3_bucket: Option<String>,
 }
 
 #[derive(Deserialize, Debug, Clone, Default)]
+#[serde(default)]
 pub struct ConfigServiceInfo {
   pub htsget_id: Option<String>,
   pub htsget_name: Option<String>,
@@ -94,13 +96,13 @@ impl Default for Config {
   fn default() -> Self {
     Self {
       htsget_addr: default_addr(),
-      htsget_resolver: default_resolver(),
+      htsget_resolver: RegexResolver::default(),
       htsget_path: default_path(),
       htsget_config_service_info: ConfigServiceInfo::default(),
-      htsget_localstorage_addr: default_localstorage_addr(),
-      htsget_localstorage_cert: default_localstorage_cert(),
-      htsget_localstorage_key: default_localstorage_key(),
       htsget_storage_type: LocalStorage,
+      htsget_localstorage_addr: default_localstorage_addr(),
+      htsget_localstorage_key: default_localstorage_key(),
+      htsget_localstorage_cert: default_localstorage_cert(),
       #[cfg(feature = "s3-storage")]
       htsget_s3_bucket: None,
     }
@@ -115,5 +117,46 @@ impl Config {
         format!("Config not properly set: {}", err),
       )
     })
+  }
+}
+
+mod tests {
+  use crate::config::Config;
+  use crate::config::StorageType::AwsS3Storage;
+
+
+  #[test]
+  fn config_addr() {
+    std::env::set_var("HTSGET_ADDR", "127.0.0.1:8081");
+    let config = Config::from_env().unwrap();
+    assert_eq!(config.htsget_addr, "127.0.0.1:8081".parse().unwrap());
+  }
+
+  #[test]
+  fn config_regex() {
+    std::env::set_var("HTSGET_REGEX", ".+");
+    let config = Config::from_env().unwrap();
+    assert_eq!(config.htsget_resolver.htsget_regex.to_string(), ".+");
+  }
+
+  #[test]
+  fn config_substitution_string() {
+    std::env::set_var("HTSGET_SUBSTITUTION_STRING", "$0-test");
+    let config = Config::from_env().unwrap();
+    assert_eq!(config.htsget_resolver.htsget_substitution_string, "$0-test");
+  }
+
+  #[test]
+  fn config_service_info_id() {
+    std::env::set_var("HTSGET_ID", "id");
+    let config = Config::from_env().unwrap();
+    assert_eq!(config.htsget_config_service_info.htsget_id.unwrap(), "id");
+  }
+
+  #[test]
+  fn config_storage_type() {
+    std::env::set_var("HTSGET_STORAGE_TYPE", "AwsS3Storage");
+    let config = Config::from_env().unwrap();
+    assert_eq!(config.htsget_storage_type, AwsS3Storage);
   }
 }
--- htsget-config/src/regex_resolver.rs
+++ htsget-config/src/regex_resolver.rs
@@ -6,32 +6,49 @@ pub trait HtsGetIdResolver {
 }
 
 #[derive(Debug, Clone, Deserialize)]
+#[serde(default)]
 pub struct RegexResolver {
   #[serde(with = "serde_regex")]
-  regex: Regex,
-  substitution_string: String,
+  pub(crate) htsget_regex: Regex,
+  pub(crate) htsget_substitution_string: String,
+}
+
+impl Default for RegexResolver {
+  fn default() -> Self {
+    Self::new(".*", "$0").expect("Expected valid resolver.")
+  }
 }
 
 impl RegexResolver {
   pub fn new(regex: &str, replacement_string: &str) -> Result<Self, Error> {
     Ok(RegexResolver {
-      regex: Regex::new(regex)?,
-      substitution_string: replacement_string.to_string(),
+      htsget_regex: Regex::new(regex)?,
+      htsget_substitution_string: replacement_string.to_string(),
     })
   }
 }
 
 impl HtsGetIdResolver for RegexResolver {
   fn resolve_id(&self, id: &str) -> Option<String> {
-    if self.regex.is_match(id) {
+    if self.htsget_regex.is_match(id) {
       Some(
         self
-          .regex
-          .replace(id, &self.substitution_string)
+          .htsget_regex
+          .replace(id, &self.htsget_substitution_string)
           .to_string(),
       )
     } else {
       None
     }
+  }
+}
+
+pub mod tests {
+  use crate::regex_resolver::{HtsGetIdResolver, RegexResolver};
+
+  #[test]
+  fn resolver_resolve_id() {
+    let resolver = RegexResolver::new(".*", "$0-test").unwrap();
+    assert_eq!(resolver.resolve_id("id").unwrap(), "id-test");
   }
 }
