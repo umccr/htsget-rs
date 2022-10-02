0f9cde9846c5b6994c1dcf695b5a85603c8bdc1c
Marko Malenic <mmalenic1@gmail.com>
2022-10-03 10:17:44 +1100

    Add base path only function in tests for code reuse

--- htsget-search/src/storage/local.rs
+++ htsget-search/src/storage/local.rs
@@ -261,8 +261,12 @@ pub(crate) mod tests {
     .await;
   }
 
+  pub(crate) fn create_base_path() -> TempDir {
+    TempDir::new().unwrap()
+  }
+
   pub(crate) async fn create_local_test_files() -> (String, TempDir) {
-    let base_path = TempDir::new().unwrap();
+    let base_path = create_base_path();
 
     let folder_name = "folder";
     let key1 = "key1";
