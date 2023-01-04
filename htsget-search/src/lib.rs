464ece815aaac08b9860805af9ab8cb8524cc0ff
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-01-04 19:44:36 +1100

    Remove project contents from benchmarks/static gh-pages branch

--- htsget-search/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub use htsget_config::config::{Config, DataServerConfig, ServiceInfo, TicketServerConfig};
-#[cfg(feature = "s3-storage")]
-pub use htsget_config::regex_resolver::aws::S3Resolver;
-pub use htsget_config::regex_resolver::{
-  LocalResolver, QueryAllowed, RegexResolver, Resolver, StorageType,
-};
-
-pub mod htsget;
-pub mod storage;
