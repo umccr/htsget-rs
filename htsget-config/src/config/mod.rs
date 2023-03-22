797643f84408e46699b927ab69b9db07a42bbcbb
Marko Malenic <mmalenic1@gmail.com>
2023-03-23 10:54:58 +1100

    feat(config): add generalized parser for figment config

--- htsget-config/src/config/mod.rs
+++ htsget-config/src/config/mod.rs
@@ -5,30 +5,27 @@ use std::net::SocketAddr;
 use std::path::{Path, PathBuf};
 
 use clap::Parser;
-use figment::providers::{Env, Format, Serialized, Toml};
-use figment::Figment;
 use http::header::HeaderName;
 use http::Method;
 use serde::{Deserialize, Serialize};
 use serde_with::with_prefix;
-use tracing::info;
 use tracing::instrument;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{fmt, EnvFilter, Registry};
 
 use crate::config::cors::{AllowType, CorsConfig, HeaderValue, TaggedAllowTypes};
+use crate::config::parser::from_path;
 use crate::resolver::Resolver;
 use crate::types::Scheme;
 use crate::types::Scheme::{Http, Https};
 
 pub mod cors;
+pub mod parser;
 
 /// Represents a usage string for htsget-rs.
 pub const USAGE: &str =
   "htsget-rs can be configured using a config file or environment variables. \
 See the documentation of the htsget-config crate for more information.";
-
-const ENVIRONMENT_VARIABLE_PREFIX: &str = "HTSGET_";
 
 pub(crate) fn default_localstorage_addr() -> &'static str {
   "127.0.0.1:8081"
@@ -474,13 +471,7 @@ impl Config {
   /// Read a config struct from a TOML file.
   #[instrument]
   pub fn from_path(path: &Path) -> io::Result<Self> {
-    let config: Config = Figment::from(Serialized::defaults(Config::default()))
-      .merge(Toml::file(path))
-      .merge(Env::prefixed(ENVIRONMENT_VARIABLE_PREFIX))
-      .extract()
-      .map_err(|err| io::Error::new(ErrorKind::Other, format!("failed to parse config: {err}")))?;
-
-    info!(config = ?config, "config created from environment variables");
+    let config: Self = from_path(path)?;
 
     Ok(config.resolvers_from_data_server_config())
   }
@@ -546,6 +537,7 @@ impl Config {
 pub(crate) mod tests {
   use std::fmt::Display;
 
+  use crate::config::parser::from_str;
   use figment::Jail;
   use http::uri::Authority;
 
@@ -557,18 +549,32 @@ pub(crate) mod tests {
   where
     K: AsRef<str>,
     V: Display,
-    F: FnOnce(Config),
+    F: Fn(Config),
   {
     Jail::expect_with(|jail| {
+      let file = "test.toml";
+
       if let Some(contents) = contents {
-        jail.create_file("test.toml", contents)?;
+        jail.create_file(file, contents)?;
       }
 
       for (key, value) in env_variables {
         jail.set_env(key, value);
       }
 
-      test_fn(Config::from_path(Path::new("test.toml")).map_err(|err| err.to_string())?);
+      let path = Path::new(file);
+      test_fn(Config::from_path(path).map_err(|err| err.to_string())?);
+
+      test_fn(
+        from_path::<Config>(path)
+          .map_err(|err| err.to_string())?
+          .resolvers_from_data_server_config(),
+      );
+      test_fn(
+        from_str::<Config>(contents.unwrap_or(""))
+          .map_err(|err| err.to_string())?
+          .resolvers_from_data_server_config(),
+      );
 
       Ok(())
     });
@@ -578,14 +584,14 @@ pub(crate) mod tests {
   where
     K: AsRef<str>,
     V: Display,
-    F: FnOnce(Config),
+    F: Fn(Config),
   {
     test_config(None, env_variables, test_fn);
   }
 
   pub(crate) fn test_config_from_file<F>(contents: &str, test_fn: F)
   where
-    F: FnOnce(Config),
+    F: Fn(Config),
   {
     test_config(Some(contents), Vec::<(&str, &str)>::new(), test_fn);
   }
---
+++ htsget-config/src/config/parser.rs
@@ -0,0 +1,58 @@
+use crate::config::Config;
+use figment::providers::{Env, Format, Serialized, Toml};
+use figment::Figment;
+use serde::Deserialize;
+use std::fmt::Debug;
+use std::io;
+use std::io::ErrorKind;
+use std::path::Path;
+use tracing::{info, instrument};
+
+const ENVIRONMENT_VARIABLE_PREFIX: &str = "HTSGET_";
+
+/// A struct to represent a string or a path, used for parsing and deserializing config.
+#[derive(Debug)]
+pub enum Parser<'a> {
+  String(&'a str),
+  Path(&'a Path),
+}
+
+impl<'a> Parser<'a> {
+  /// Deserialize a string or path into a config value using Figment.
+  #[instrument]
+  pub fn deserialize_config_into<T>(&self) -> io::Result<T>
+  where
+    for<'de> T: Deserialize<'de> + Debug,
+  {
+    let config: T = Figment::from(Serialized::defaults(Config::default()))
+      .merge(match self {
+        Parser::String(string) => Toml::string(string),
+        Parser::Path(path) => Toml::file(path),
+      })
+      .merge(Env::prefixed(ENVIRONMENT_VARIABLE_PREFIX))
+      .extract()
+      .map_err(|err| io::Error::new(ErrorKind::Other, format!("failed to parse config: {err}")))?;
+
+    info!(config = ?config, "config created");
+
+    Ok(config)
+  }
+}
+
+/// Read a deserializable config struct from a TOML file.
+#[instrument]
+pub fn from_path<T>(path: &Path) -> io::Result<T>
+where
+  for<'a> T: Deserialize<'a> + Debug,
+{
+  Parser::Path(path).deserialize_config_into()
+}
+
+/// Read a deserializable config struct from a str.
+#[instrument]
+pub fn from_str<T>(str: &str) -> io::Result<T>
+where
+  for<'a> T: Deserialize<'a> + Debug,
+{
+  Parser::String(str).deserialize_config_into()
+}
