2bb6c53cb4f912d3b242be7a7702757d3a346570
Marko Malenic <mmalenic1@gmail.com>
2023-08-16 11:27:52 +1000

    refactor(config)!: split resolver module into multiple files

---
+++ htsget-config/src/resolver/allow_guard.rs
@@ -0,0 +1,176 @@
+use std::collections::HashSet;
+
+use serde::{Deserialize, Serialize};
+use serde_with::with_prefix;
+
+use crate::types::Format::{Bam, Bcf, Cram, Vcf};
+use crate::types::{Class, Fields, Format, Interval, Query, TaggedTypeAll, Tags};
+
+/// Determines whether the query matches for use with the storage.
+pub trait QueryAllowed {
+  /// Does this query match.
+  fn query_allowed(&self, query: &Query) -> bool;
+}
+
+with_prefix!(allow_interval_prefix "allow_interval_");
+
+/// A query guard represents query parameters that can be allowed to storage for a given query.
+#[derive(Serialize, Clone, Debug, Deserialize, PartialEq, Eq)]
+#[serde(default)]
+pub struct AllowGuard {
+  allow_reference_names: ReferenceNames,
+  allow_fields: Fields,
+  allow_tags: Tags,
+  allow_formats: Vec<Format>,
+  allow_classes: Vec<Class>,
+  #[serde(flatten, with = "allow_interval_prefix")]
+  allow_interval: Interval,
+}
+
+/// Reference names that can be matched.
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(untagged)]
+pub enum ReferenceNames {
+  Tagged(TaggedTypeAll),
+  List(HashSet<String>),
+}
+
+impl AllowGuard {
+  /// Create a new allow guard.
+  pub fn new(
+    allow_reference_names: ReferenceNames,
+    allow_fields: Fields,
+    allow_tags: Tags,
+    allow_formats: Vec<Format>,
+    allow_classes: Vec<Class>,
+    allow_interval: Interval,
+  ) -> Self {
+    Self {
+      allow_reference_names,
+      allow_fields,
+      allow_tags,
+      allow_formats,
+      allow_classes,
+      allow_interval,
+    }
+  }
+
+  /// Get allow formats.
+  pub fn allow_formats(&self) -> &[Format] {
+    &self.allow_formats
+  }
+
+  /// Get allow classes.
+  pub fn allow_classes(&self) -> &[Class] {
+    &self.allow_classes
+  }
+
+  /// Get allow interval.
+  pub fn allow_interval(&self) -> Interval {
+    self.allow_interval
+  }
+
+  /// Get allow reference names.
+  pub fn allow_reference_names(&self) -> &ReferenceNames {
+    &self.allow_reference_names
+  }
+
+  /// Get allow fields.
+  pub fn allow_fields(&self) -> &Fields {
+    &self.allow_fields
+  }
+
+  /// Get allow tags.
+  pub fn allow_tags(&self) -> &Tags {
+    &self.allow_tags
+  }
+}
+
+impl Default for AllowGuard {
+  fn default() -> Self {
+    Self {
+      allow_formats: vec![Bam, Cram, Vcf, Bcf],
+      allow_classes: vec![Class::Body, Class::Header],
+      allow_interval: Default::default(),
+      allow_reference_names: ReferenceNames::Tagged(TaggedTypeAll::All),
+      allow_fields: Fields::Tagged(TaggedTypeAll::All),
+      allow_tags: Tags::Tagged(TaggedTypeAll::All),
+    }
+  }
+}
+
+impl QueryAllowed for ReferenceNames {
+  fn query_allowed(&self, query: &Query) -> bool {
+    match (self, &query.reference_name()) {
+      (ReferenceNames::Tagged(TaggedTypeAll::All), _) => true,
+      (ReferenceNames::List(reference_names), Some(reference_name)) => {
+        reference_names.contains(*reference_name)
+      }
+      (ReferenceNames::List(_), None) => false,
+    }
+  }
+}
+
+impl QueryAllowed for Fields {
+  fn query_allowed(&self, query: &Query) -> bool {
+    match (self, &query.fields()) {
+      (Fields::Tagged(TaggedTypeAll::All), _) => true,
+      (Fields::List(self_fields), Fields::List(query_fields)) => {
+        self_fields.is_subset(query_fields)
+      }
+      (Fields::List(_), Fields::Tagged(TaggedTypeAll::All)) => false,
+    }
+  }
+}
+
+impl QueryAllowed for Tags {
+  fn query_allowed(&self, query: &Query) -> bool {
+    match (self, &query.tags()) {
+      (Tags::Tagged(TaggedTypeAll::All), _) => true,
+      (Tags::List(self_tags), Tags::List(query_tags)) => self_tags.is_subset(query_tags),
+      (Tags::List(_), Tags::Tagged(TaggedTypeAll::All)) => false,
+    }
+  }
+}
+
+impl QueryAllowed for AllowGuard {
+  fn query_allowed(&self, query: &Query) -> bool {
+    self.allow_formats.contains(&query.format())
+      && self.allow_classes.contains(&query.class())
+      && self
+        .allow_interval
+        .contains(query.interval().start().unwrap_or(u32::MIN))
+      && self
+        .allow_interval
+        .contains(query.interval().end().unwrap_or(u32::MAX))
+      && self.allow_reference_names.query_allowed(query)
+      && self.allow_fields.query_allowed(query)
+      && self.allow_tags.query_allowed(query)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::config::tests::test_config_from_file;
+
+  use super::*;
+
+  #[test]
+  fn config_resolvers_guard_file() {
+    test_config_from_file(
+      r#"
+        [[resolvers]]
+        regex = "regex"
+
+        [resolvers.allow_guard]
+        allow_formats = ["BAM"]
+        "#,
+      |config| {
+        assert_eq!(
+          config.resolvers().first().unwrap().allow_formats(),
+          &vec![Bam]
+        );
+      },
+    );
+  }
+}
--- htsget-config/src/resolver/mod.rs
+++ htsget-config/src/resolver/mod.rs
@@ -1,21 +1,21 @@
-use std::collections::HashSet;
+pub mod allow_guard;
+
 use std::result;
 
 use async_trait::async_trait;
 use regex::{Error, Regex};
 use serde::{Deserialize, Serialize};
-use serde_with::with_prefix;
 use tracing::instrument;
 
 use crate::config::DataServerConfig;
+use crate::resolver::allow_guard::{AllowGuard, QueryAllowed, ReferenceNames};
 use crate::storage::local::LocalStorage;
 #[cfg(feature = "s3-storage")]
 use crate::storage::s3::S3Storage;
 #[cfg(feature = "url-storage")]
 use crate::storage::url::UrlStorage;
 use crate::storage::{ResolvedId, Storage, TaggedStorageTypes};
-use crate::types::Format::{Bam, Bcf, Cram, Vcf};
-use crate::types::{Class, Fields, Format, Interval, Query, Response, Result, TaggedTypeAll, Tags};
+use crate::types::{Class, Fields, Format, Interval, Query, Response, Result, Tags};
 
 /// A trait which matches the query id, replacing the match in the substitution text.
 pub trait IdResolver {
@@ -46,12 +46,6 @@ pub trait StorageResolver {
     &self,
     query: &mut Query,
   ) -> Option<Result<Response>>;
-}
-
-/// Determines whether the query matches for use with the storage.
-pub trait QueryAllowed {
-  /// Does this query match.
-  fn query_allowed(&self, query: &Query) -> bool;
 }
 
 /// A regex storage is a storage that matches ids using Regex.
@@ -93,142 +87,19 @@ impl<T> ResolvedStorage<T> {
   }
 }
 
-impl ResolvedId {}
-
-with_prefix!(allow_interval_prefix "allow_interval_");
-
-/// A query guard represents query parameters that can be allowed to storage for a given query.
-#[derive(Serialize, Clone, Debug, Deserialize, PartialEq, Eq)]
-#[serde(default)]
-pub struct AllowGuard {
-  allow_reference_names: ReferenceNames,
-  allow_fields: Fields,
-  allow_tags: Tags,
-  allow_formats: Vec<Format>,
-  allow_classes: Vec<Class>,
-  #[serde(flatten, with = "allow_interval_prefix")]
-  allow_interval: Interval,
-}
-
-/// Reference names that can be matched.
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-#[serde(untagged)]
-pub enum ReferenceNames {
-  Tagged(TaggedTypeAll),
-  List(HashSet<String>),
-}
-
-impl AllowGuard {
-  /// Create a new allow guard.
-  pub fn new(
-    allow_reference_names: ReferenceNames,
-    allow_fields: Fields,
-    allow_tags: Tags,
-    allow_formats: Vec<Format>,
-    allow_classes: Vec<Class>,
-    allow_interval: Interval,
-  ) -> Self {
-    Self {
-      allow_reference_names,
-      allow_fields,
-      allow_tags,
-      allow_formats,
-      allow_classes,
-      allow_interval,
+impl IdResolver for Resolver {
+  #[instrument(level = "trace", skip(self), ret)]
+  fn resolve_id(&self, query: &Query) -> Option<ResolvedId> {
+    if self.regex.is_match(query.id()) && self.allow_guard.query_allowed(query) {
+      Some(ResolvedId::new(
+        self
+          .regex
+          .replace(query.id(), &self.substitution_string)
+          .to_string(),
+      ))
+    } else {
+      None
     }
-  }
-
-  /// Get allow formats.
-  pub fn allow_formats(&self) -> &[Format] {
-    &self.allow_formats
-  }
-
-  /// Get allow classes.
-  pub fn allow_classes(&self) -> &[Class] {
-    &self.allow_classes
-  }
-
-  /// Get allow interval.
-  pub fn allow_interval(&self) -> Interval {
-    self.allow_interval
-  }
-
-  /// Get allow reference names.
-  pub fn allow_reference_names(&self) -> &ReferenceNames {
-    &self.allow_reference_names
-  }
-
-  /// Get allow fields.
-  pub fn allow_fields(&self) -> &Fields {
-    &self.allow_fields
-  }
-
-  /// Get allow tags.
-  pub fn allow_tags(&self) -> &Tags {
-    &self.allow_tags
-  }
-}
-
-impl Default for AllowGuard {
-  fn default() -> Self {
-    Self {
-      allow_formats: vec![Bam, Cram, Vcf, Bcf],
-      allow_classes: vec![Class::Body, Class::Header],
-      allow_interval: Default::default(),
-      allow_reference_names: ReferenceNames::Tagged(TaggedTypeAll::All),
-      allow_fields: Fields::Tagged(TaggedTypeAll::All),
-      allow_tags: Tags::Tagged(TaggedTypeAll::All),
-    }
-  }
-}
-
-impl QueryAllowed for ReferenceNames {
-  fn query_allowed(&self, query: &Query) -> bool {
-    match (self, &query.reference_name()) {
-      (ReferenceNames::Tagged(TaggedTypeAll::All), _) => true,
-      (ReferenceNames::List(reference_names), Some(reference_name)) => {
-        reference_names.contains(*reference_name)
-      }
-      (ReferenceNames::List(_), None) => false,
-    }
-  }
-}
-
-impl QueryAllowed for Fields {
-  fn query_allowed(&self, query: &Query) -> bool {
-    match (self, &query.fields()) {
-      (Fields::Tagged(TaggedTypeAll::All), _) => true,
-      (Fields::List(self_fields), Fields::List(query_fields)) => {
-        self_fields.is_subset(query_fields)
-      }
-      (Fields::List(_), Fields::Tagged(TaggedTypeAll::All)) => false,
-    }
-  }
-}
-
-impl QueryAllowed for Tags {
-  fn query_allowed(&self, query: &Query) -> bool {
-    match (self, &query.tags()) {
-      (Tags::Tagged(TaggedTypeAll::All), _) => true,
-      (Tags::List(self_tags), Tags::List(query_tags)) => self_tags.is_subset(query_tags),
-      (Tags::List(_), Tags::Tagged(TaggedTypeAll::All)) => false,
-    }
-  }
-}
-
-impl QueryAllowed for AllowGuard {
-  fn query_allowed(&self, query: &Query) -> bool {
-    self.allow_formats.contains(&query.format())
-      && self.allow_classes.contains(&query.class())
-      && self
-        .allow_interval
-        .contains(query.interval().start().unwrap_or(u32::MIN))
-      && self
-        .allow_interval
-        .contains(query.interval().end().unwrap_or(u32::MAX))
-      && self.allow_reference_names.query_allowed(query)
-      && self.allow_fields.query_allowed(query)
-      && self.allow_tags.query_allowed(query)
   }
 }
 
@@ -306,38 +177,22 @@ impl Resolver {
 
   /// Get allow interval.
   pub fn allow_interval(&self) -> Interval {
-    self.allow_guard.allow_interval
+    self.allow_guard.allow_interval()
   }
 
   /// Get allow reference names.
   pub fn allow_reference_names(&self) -> &ReferenceNames {
-    &self.allow_guard.allow_reference_names
+    self.allow_guard.allow_reference_names()
   }
 
   /// Get allow fields.
   pub fn allow_fields(&self) -> &Fields {
-    &self.allow_guard.allow_fields
+    self.allow_guard.allow_fields()
   }
 
   /// Get allow tags.
   pub fn allow_tags(&self) -> &Tags {
-    &self.allow_guard.allow_tags
-  }
-}
-
-impl IdResolver for Resolver {
-  #[instrument(level = "trace", skip(self), ret)]
-  fn resolve_id(&self, query: &Query) -> Option<ResolvedId> {
-    if self.regex.is_match(query.id()) && self.allow_guard.query_allowed(query) {
-      Some(ResolvedId::new(
-        self
-          .regex
-          .replace(query.id(), &self.substitution_string)
-          .to_string(),
-      ))
-    } else {
-      None
-    }
+    self.allow_guard.allow_tags()
   }
 }
 
@@ -414,10 +269,11 @@ mod tests {
   };
 
   use crate::config::tests::{test_config_from_env, test_config_from_file};
-  #[cfg(feature = "s3-storage")]
-  use crate::storage::s3::S3Storage;
+  use crate::types::Format::Bam;
   use crate::types::Scheme::Http;
   use crate::types::Url;
+  #[cfg(feature = "s3-storage")]
+  use {crate::storage::s3::S3Storage, std::collections::HashSet};
 
   use super::*;
 
@@ -597,25 +453,6 @@ mod tests {
         assert_eq!(
           config.resolvers().first().unwrap().regex().as_str(),
           "regex"
-        );
-      },
-    );
-  }
-
-  #[test]
-  fn config_resolvers_guard_file() {
-    test_config_from_file(
-      r#"
-      [[resolvers]]
-      regex = "regex"
-
-      [resolvers.allow_guard]
-      allow_formats = ["BAM"]
-      "#,
-      |config| {
-        assert_eq!(
-          config.resolvers().first().unwrap().allow_formats(),
-          &vec![Bam]
         );
       },
     );
