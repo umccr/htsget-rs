db31d04d08c7cd1822c1fbef885741f8cac74fe7
Marko Malenic <mmalenic1@gmail.com>
2023-11-02 11:34:34 +0100

    feat(search): unencrypted byte ranges working

--- htsget-search/src/storage/url.rs
+++ htsget-search/src/storage/url.rs
@@ -89,7 +89,8 @@ impl UrlStorage {
 
   /// Get a url from the key.
   pub fn get_url_from_key<K: AsRef<str> + Send>(&self, key: K, endpoint: &Uri) -> Result<Uri> {
-    let uri = if endpoint.to_string().ends_with("/") {
+    // Todo: proper url parsing here, probably with the `url` crate.
+    let uri = if endpoint.to_string().ends_with('/') {
       format!("{}{}", endpoint, key.as_ref())
     } else {
       format!("{}/{}", endpoint, key.as_ref())
@@ -308,27 +309,29 @@ mod tests {
   use std::result;
   use std::str::FromStr;
 
+  use axum::body::StreamBody;
+  use axum::extract::Path as AxumPath;
   use axum::middleware::Next;
   use axum::response::{IntoResponse, Response};
-  use axum::{middleware, Router};
-  use axum::body::StreamBody;
   use axum::routing::{get, head};
+  use axum::{middleware, Router};
   use http::header::AUTHORIZATION;
   use http::{HeaderName, HeaderValue, Request, StatusCode};
   use hyper::body::to_bytes;
-  use noodles::{bam, sam};
-  use crate::Response as HtsgetResponse;
   use tokio::fs::File;
   use tokio::io::AsyncReadExt;
   use tokio_util::io::ReaderStream;
   use tower_http::services::ServeDir;
 
-  use htsget_config::types::{Format, Headers, Query, Url};
   use htsget_config::types::Class::{Body, Header};
+  use htsget_config::types::Request as HtsgetRequest;
+  use htsget_config::types::{Format, Headers, Query, Url};
+  use htsget_test::http_tests::default_dir;
+
   use crate::htsget::from_storage::HtsGetFromStorage;
   use crate::htsget::HtsGet;
-
   use crate::storage::local::tests::create_local_test_files;
+  use crate::Response as HtsgetResponse;
 
   use super::*;
 
@@ -658,13 +661,18 @@ mod tests {
         Uri::from_str("http://example.com").unwrap(),
         true,
         #[cfg(feature = "crypt4gh")]
-          Some(Uri::from_str(&format!("{}/endpoint_crypt4gh_header", url)).unwrap()),
+        None,
       );
 
-      let query = Query::new_with_default_request("htsnexus_test_NA12878", Format::Bam)
+      let mut header_map = HeaderMap::default();
+      test_headers(&mut header_map);
+      let request =
+        HtsgetRequest::new_with_id("htsnexus_test_NA12878".to_string()).with_headers(header_map);
+      let query = Query::new("htsnexus_test_NA12878", Format::Bam, request)
         .with_reference_name("11")
         .with_start(5015000)
         .with_end(5050000);
+
       let searcher = HtsGetFromStorage::new(storage);
       let response = searcher.search(query.clone()).await;
 
@@ -672,30 +680,32 @@ mod tests {
         Format::Bam,
         vec![
           Url::new("http://example.com/htsnexus_test_NA12878.bam")
-            .with_headers(Headers::default().with_header("Range", "bytes=0-4667"))
+            .with_headers(
+              Headers::default()
+                .with_header("authorization", "secret")
+                .with_header("Range", "bytes=0-4667"),
+            )
             .with_class(Header),
           Url::new("http://example.com/htsnexus_test_NA12878.bam")
-            .with_headers(Headers::default().with_header("Range", "bytes=256721-1065951"))
+            .with_headers(
+              Headers::default()
+                .with_header("authorization", "secret")
+                .with_header("Range", "bytes=256721-1065951"),
+            )
             .with_class(Body),
           Url::new("http://example.com/htsnexus_test_NA12878.bam")
-            .with_headers(Headers::default().with_header("Range", "bytes=2596771-2596798"))
+            .with_headers(
+              Headers::default()
+                .with_header("authorization", "secret")
+                .with_header("Range", "bytes=2596771-2596798"),
+            )
             .with_class(Body),
         ],
       ));
       assert_eq!(response, expected_response);
-
-      // let mut headers = HeaderMap::default();
-      // let options = test_range_options(&mut headers);
-      //
-      // assert_eq!(
-      //   storage.range_url("assets/key1", options).await.unwrap(),
-      //   HtsGetUrl::new(format!("{}/assets/key1", url))
-      //     .with_headers(Headers::default().with_header(AUTHORIZATION.as_str(), "secret"))
-      // );
     })
-      .await;
+    .await;
   }
-
 
   fn test_client() -> Client<HttpsConnector<HttpConnector>> {
     Client::builder().build(
@@ -738,46 +748,78 @@ mod tests {
     Fut: Future<Output = ()>,
   {
     let mut router = Router::new()
-      .route("/endpoint_file/:id", get(|| async {
-        let mut bytes = vec![];
-        File::open("data/bam/htsnexus_test_NA12878.bam").await.unwrap().read_to_end(&mut bytes).await.unwrap();
+      .route(
+        "/endpoint_file/:id",
+        get(|| async {
+          let mut bytes = vec![];
+          let path = default_dir().join("data/bam/htsnexus_test_NA12878.bam");
+          File::open(path)
+            .await
+            .unwrap()
+            .read_to_end(&mut bytes)
+            .await
+            .unwrap();
 
-        let bytes = bytes[..4668].to_vec();
+          // let bytes = bytes[..4668].to_vec();
 
-        let stream = ReaderStream::new(Cursor::new(bytes));
-        let body = StreamBody::new(stream);
+          let stream = ReaderStream::new(Cursor::new(bytes));
+          let body = StreamBody::new(stream);
 
-        (StatusCode::OK, body).into_response()
-      }))
-      .route("/endpoint_index/:id", get(|| async {
-        let mut bytes = vec![];
-        File::open("data/bam/htsnexus_test_NA12878.bam.bai").await.unwrap().read_to_end(&mut bytes).await.unwrap();
+          (StatusCode::OK, body).into_response()
+        }),
+      )
+      .route(
+        "/endpoint_index/:id",
+        get(|AxumPath(id): AxumPath<String>| async move {
+          if id == "htsnexus_test_NA12878.bam.bai" {
+            let mut bytes = vec![];
+            let path = default_dir().join("data/bam/htsnexus_test_NA12878.bam.bai");
+            File::open(path)
+              .await
+              .unwrap()
+              .read_to_end(&mut bytes)
+              .await
+              .unwrap();
 
-        let stream = ReaderStream::new(Cursor::new(bytes));
-        let body = StreamBody::new(stream);
+            let stream = ReaderStream::new(Cursor::new(bytes));
+            let body = StreamBody::new(stream);
 
-        (StatusCode::OK, body).into_response()
-      }))
-      .route("/endpoint_head/:id", head(|| async {
-        let mut headers = HeaderMap::new();
-        headers.insert("Content-Length", HeaderValue::from_static("2596799"));
+            (StatusCode::OK, body).into_response()
+          } else {
+            let bytes: Vec<u8> = vec![];
+            let stream = ReaderStream::new(Cursor::new(bytes));
+            let body = StreamBody::new(stream);
 
-        (StatusCode::OK, headers).into_response()
-      }))
+            (StatusCode::NOT_FOUND, body).into_response()
+          }
+        }),
+      )
+      .route(
+        "/endpoint_head/:id",
+        head(|| async {
+          let mut headers = HeaderMap::new();
+          headers.insert("Content-Length", HeaderValue::from_static("2596799"));
+
+          (StatusCode::OK, headers).into_response()
+        }),
+      )
       .nest_service("/assets", ServeDir::new(server_base_path.to_str().unwrap()))
       .route_layer(middleware::from_fn(test_auth));
 
     #[cfg(feature = "crypt4gh")]
     {
-      router = router.route("/endpoint_crypt4gh_header/:id", head(|| async {
-        let length: u64 = 124;
-        let bytes = length.to_le_bytes().to_vec();
+      router = router.route(
+        "/endpoint_crypt4gh_header/:id",
+        head(|| async {
+          let length: u64 = 124;
+          let bytes = length.to_le_bytes().to_vec();
 
-        let stream = ReaderStream::new(Cursor::new(bytes));
-        let body = StreamBody::new(stream);
+          let stream = ReaderStream::new(Cursor::new(bytes));
+          let body = StreamBody::new(stream);
 
-        (StatusCode::OK, body).into_response()
-      }));
+          (StatusCode::OK, body).into_response()
+        }),
+      );
     }
 
     // TODO fix this in htsget-test to bind and return tcp listener.
