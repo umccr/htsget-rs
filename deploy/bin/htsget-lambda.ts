0aa42abfbe89611bb9267760c689e2809ef70912
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2024-10-17 16:29:00 +1100

    Add app to the test stack

--- deploy/bin/htsget-lambda.ts
+++ deploy/bin/htsget-lambda.ts
@@ -1,19 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {HtsgetLambdaConstruct} from "../../deploy/lib/htsget-lambda-construct";
+import { SETTINGS } from "../../deploy/bin/settings"
+import { HtsgetSettings } from "../../deploy/lib/htsget-lambda-construct"
 
 export class HtsgetTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, settings: HtsgetSettings, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new HtsgetLambdaConstruct(this, 'Htsget-rs', {
-      config: "",
-      domain: "",
-      lookupHostedZone: true,
-      s3BucketResources: [],
-      jwtAuthorizer: {
-        public: false,
-      },
-    });
+    new HtsgetLambdaConstruct(this, 'Htsget-rs', SETTINGS);
   }
 }
+
+const app = new cdk.App();
+new HtsgetTestStack(
+  app,
+  "HtsgetTestStack",
+  SETTINGS,
+  {
+    stackName: "HtsgetTestStack",
+    description: "HtsgetTestStack",
+    tags: {
+      Stack: "HtsgetTestStack",
+    },
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  },
+);
