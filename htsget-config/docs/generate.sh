e160a08c9813290c6a7ffed36ab51639c8b5087a
mmalenic <mmalenic1@gmail.com>
2025-08-04 14:18:44 +1000

    ci: add pre-commit after testing doc generation

--- htsget-config/docs/generate.sh
+++ htsget-config/docs/generate.sh
@@ -5,3 +5,4 @@ set -euxo pipefail
 # Update the default config toml and authorization response schema.
 cargo run -p htsget-axum -- -p > examples/default.toml
 cargo run -p htsget-axum -- -s > schemas/auth.schema.json
+pre-commit run --files schemas/auth.schema.json examples/default.toml || true
