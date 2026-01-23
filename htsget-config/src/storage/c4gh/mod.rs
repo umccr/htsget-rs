ab6257d48258224420ad0f4578f61c8854663811
mmalenic <mmalenic1@gmail.com>
2026-01-23 17:25:10 +1100

    feat(config): allow different C4GH keys for the client and server to be configured

--- htsget-config/src/storage/c4gh/mod.rs
+++ htsget-config/src/storage/c4gh/mod.rs
@@ -22,18 +22,20 @@ pub mod secrets_manager;
 
 /// Specifies the location of a Crypt4GH key.
 #[derive(JsonSchema, Deserialize, Debug, Clone)]
-#[serde(try_from = "C4GHKeyLocation", deny_unknown_fields)]
+#[serde(try_from = "C4GHKeySet", deny_unknown_fields)]
 pub struct C4GHKeys {
+  // Store a cloneable futures so that they can be resolved outside serde.
   #[schemars(with = "C4GHKeyLocation")]
-  // Store a cloneable future so that it can be resolved outside serde.
-  keys: Shared<BoxFuture<'static, Result<Vec<crypt4gh::Keys>>>>,
-  using_header: Option<C4GHHeader>,
+  server_decryption_keys: Shared<BoxFuture<'static, Result<Vec<crypt4gh::Keys>>>>,
+  #[schemars(with = "C4GHKeyLocation")]
+  client_encryption_keys: Shared<BoxFuture<'static, Result<Vec<crypt4gh::Keys>>>>,
+  client_key_from_header: Option<C4GHHeader>,
 }
 
 impl C4GHKeys {
   /// Get the inner values.
-  pub async fn into_inner(self) -> Result<(Vec<crypt4gh::Keys>, Option<C4GHHeader>)> {
-    Ok((self.keys.await?, self.using_header))
+  pub async fn into_inner(self) -> Result<(Vec<crypt4gh::Keys>, Vec<crypt4gh::Keys>, Option<C4GHHeader>)> {
+    Ok((self.server_decryption_keys.await?, self.client_encryption_keys.await?, self.client_key_from_header))
   }
 
   /// Construct the C4GH keys from a key pair.
@@ -47,12 +49,14 @@ impl C4GHKeys {
 
   /// Construct from an existing join handle.
   pub fn from_join_handle(
-    handle: JoinHandle<Result<Vec<crypt4gh::Keys>>>,
-    using_header: Option<C4GHHeader>,
+    server_keys: JoinHandle<Result<Vec<crypt4gh::Keys>>>,
+    client_keys: JoinHandle<Result<Vec<crypt4gh::Keys>>>,
+    client_key_from_header: Option<C4GHHeader>,
   ) -> Self {
     Self {
-      keys: handle.map(|value| value?).boxed().shared(),
-      using_header,
+      server_decryption_keys: server_keys.map(|value| value?).boxed().shared(),
+      client_encryption_keys: client_keys.map(|value| value?).boxed().shared(),
+      client_key_from_header,
     }
   }
 }
@@ -97,54 +101,89 @@ impl C4GHKeyType {
   pub fn new_secrets_manager(secrets_manager: C4GHSecretsManager) -> Self {
     Self::SecretsManager(secrets_manager)
   }
+
+  /// Create a key type for obtaining keys from a header.
+  pub fn new_header(header: C4GHHeader) -> Self {
+    Self::Header(header)
+  }
 }
 
+/// The specific location for a private and public key pair. If the private key is
+/// unspecified then the public key is used for encryption, otherwise decryption is
+/// used.
 #[derive(JsonSchema, Deserialize, Debug, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct C4GHKeyLocation {
-  private: C4GHKeyType,
+  private: Option<C4GHKeyType>,
   public: C4GHKeyType,
 }
 
 impl C4GHKeyLocation {
   /// Create a new C4GH location.
-  pub fn new(private: C4GHKeyType, public: C4GHKeyType) -> Self {
+  pub fn new(private: Option<C4GHKeyType>, public: C4GHKeyType) -> Self {
     Self { private, public }
   }
 }
 
-impl TryFrom<C4GHKeyLocation> for C4GHKeys {
+/// A keyset comprising the server's key pair and the client's public key, which will be used
+/// to re-encrypt the header for the client.
+#[derive(JsonSchema, Deserialize, Debug, Clone)]
+#[serde(deny_unknown_fields)]
+pub struct C4GHKeySet {
+  server: C4GHKeyLocation,
+  client: C4GHKeyLocation,
+}
+
+impl TryFrom<C4GHKeySet> for C4GHKeys {
   type Error = Error;
 
-  fn try_from(location: C4GHKeyLocation) -> Result<Self> {
-    let private_key: Pin<Box<dyn Future<Output = _> + Send>> = match location.private {
-      C4GHKeyType::File(file) => Box::pin(async { file.into_private_key() }),
-      #[cfg(feature = "aws")]
-      C4GHKeyType::SecretsManager(secrets_manager) => Box::pin(secrets_manager.into_private_key()),
-      C4GHKeyType::Header(_) => {
-        return Err(ParseError(
-          "using a header for private keys is unsupported".to_string(),
-        ));
+  fn try_from(location: C4GHKeySet) -> Result<Self> {
+    let extract_private_key = |private_key| -> Result<Pin<Box<dyn Future<Output = _> + Send>>> {
+      match private_key {
+        Some(C4GHKeyType::File(file)) => Ok(Box::pin(async { file.into_private_key() })),
+        #[cfg(feature = "aws")]
+        Some(C4GHKeyType::SecretsManager(secrets_manager)) => Ok(Box::pin(secrets_manager.into_private_key())),
+        Some(C4GHKeyType::Header(_)) => {
+          Err(ParseError(
+            "using a header for private keys is unsupported".to_string(),
+          ))
+        }
+        _ => {
+          Err(ParseError("missing server private key".to_string()))
+        }
       }
     };
-    let (recipient_public, using_header): (Pin<Box<dyn Future<Output = _> + Send>>, _) =
-      match location.public {
+    let extract_public_key = |public_key| -> (Pin<Box<dyn Future<Output = _> + Send>>, _) {
+      match public_key {
         C4GHKeyType::File(file) => (Box::pin(async { file.into_public_key() }), None),
         #[cfg(feature = "aws")]
         C4GHKeyType::SecretsManager(secrets_manager) => {
           (Box::pin(secrets_manager.into_public_key()), None)
         }
         C4GHKeyType::Header(using_header) => (Box::pin(async { Ok(vec![]) }), Some(using_header)),
-      };
+      }
+    };
+
+    let server_decryption_private = extract_private_key(location.server.private.clone())?;
+    let server_encryption_private = extract_private_key(location.server.private)?;
+
+    if location.client.private.is_some() {
+      return Err(ParseError("the client's private key should not be specified".to_string()));
+    }
+
+    let (server_public_key, _) = extract_public_key(location.server.public);
+    let (client_public_key, client_key_from_header) = extract_public_key(location.client.public);
 
     Ok(C4GHKeys::from_join_handle(
       tokio::spawn(async move {
-        let private_key = private_key.await?;
-        let recipient_public = recipient_public.await?;
-
-        Ok(C4GHKeys::from_key_pair(private_key, recipient_public))
+        // Server decrypts using it's own private key and public key.
+        Ok(C4GHKeys::from_key_pair(server_decryption_private.await?, server_public_key.await?))
       }),
-      using_header,
+      tokio::spawn(async move {
+        // Server encrypts using it's own private key for the client who is the recipient.
+        Ok(C4GHKeys::from_key_pair(server_encryption_private.await?, client_public_key.await?))
+      }),
+      client_key_from_header,
     ))
   }
 }
